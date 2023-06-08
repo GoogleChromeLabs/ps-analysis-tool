4cad1a578be8cae6de52b281a1dc73d2be97b241
ayushnirwal <coding.carrots@gmail.com>
2023-06-08 13:33:52 +0530

    initial scaffold

---
+++ packages/extension/src/devtools/index.tsx
@@ -0,0 +1,12 @@
+/**
+ * External dependencies.
+ */
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+/**
+ * External dependencies.
+ */
+import App from './app';
+
+createRoot(document.getElementById('root')).render(<App />);
