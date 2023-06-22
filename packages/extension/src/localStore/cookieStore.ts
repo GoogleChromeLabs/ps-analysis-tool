8256c357384eeb7e1c897b13e7b7378fabf3e4b2
ayushnirwal <coding.carrots@gmail.com>
2023-06-22 18:29:54 +0530

    fix duplicate cookies

--- packages/extension/src/localStore/cookieStore.ts
+++ packages/extension/src/localStore/cookieStore.ts
@@ -30,8 +30,7 @@ const CookieStore = {
 
     for (const cookie of cookies) {
       if (cookie) {
-        newCookies[cookie.parsedCookie.name + cookie.parsedCookie.domain] =
-          cookie;
+        newCookies[cookie.parsedCookie.name] = cookie;
       }
     }
 
