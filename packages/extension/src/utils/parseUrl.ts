0aee48e3e7888b1efbf572a4bf1e368321a7a91e
sayedtaqui <sayedwp@gmail.com>
2023-06-28 08:52:15 +0530

    Fix ts issue

--- packages/extension/src/utils/parseUrl.ts
+++ packages/extension/src/utils/parseUrl.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export const parseUrl = (url) => {
+export const parseUrl = (url: string) => {
   try {
     return new URL(url);
   } catch (e) {
