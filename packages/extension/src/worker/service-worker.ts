54818c514bf1840766e9e620f4fe52370a2b7a07
sayedtaqui <sayedwp@gmail.com>
2023-07-14 11:03:17 +0530

    Do not clear storage from service worker and add unlimitedstorage permission

--- packages/extension/src/worker/service-worker.ts
+++ packages/extension/src/worker/service-worker.ts
@@ -25,9 +25,6 @@ import {
   fetchDictionary,
 } from '../utils/fetchCookieDictionary';
 
-//clear data from previous sessions or version of the extension ( or when extension is refreshed )
-chrome.storage.local.clear();
-
 let cookieDB: CookieDatabase | null = null;
 
 /**
