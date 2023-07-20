646804adc8caf2d3b3315cd68a2d7f71de6cffcd
sayedtaqui <sayedwp@gmail.com>
2023-07-20 11:41:03 +0530

    Remove ./ from copyPlugin from and to paths

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -84,10 +84,10 @@ const root = {
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: './packages/extension/src/manifest.json', to: '' },
-        { from: './packages/extension/icons', to: 'icons' },
-        { from: './third_party/', to: './third_party' },
-        { from: './data/PSInfo.json', to: './data/PSInfo.json' },
+        { from: 'packages/extension/src/manifest.json', to: '' },
+        { from: 'packages/extension/icons', to: 'icons' },
+        { from: 'third_party/', to: 'third_party' },
+        { from: 'data/PSInfo.json', to: 'data/PSInfo.json' },
       ],
     }),
     new WebpackBar({
