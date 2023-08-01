e983816bb6ecd0a22cca12af58947934ff31b46f
Mayank Rana <mayankranax1@gmail.com>
2023-08-01 16:24:05 +0530

    Updates file path in story

--- packages/extension/src/view/devtools/components/tabContent/cookies/components/cookieDetails/stories/details.stories.ts
+++ packages/extension/src/view/devtools/components/tabContent/cookies/components/cookieDetails/stories/details.stories.ts
@@ -22,7 +22,7 @@ import type { Meta, StoryObj } from '@storybook/react';
  * Internal dependencies.
  */
 import Details from '../details';
-import type { CookieTableData } from '../../../../../../../stateProviders/syncCookieStore';
+import type { CookieTableData } from '../../../../../../stateProviders/syncCookieStore';
 
 const meta = {
   title: 'Extension/CookiesPanel/CookieDetails/Details',
