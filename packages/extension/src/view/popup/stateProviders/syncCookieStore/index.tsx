1cf515feb874b97a09929b47955d1dd39bf7054d
sayedtaqui <sayedwp@gmail.com>
2023-08-04 14:39:27 +0530

    Test commit

--- packages/extension/src/view/popup/stateProviders/syncCookieStore/index.tsx
+++ packages/extension/src/view/popup/stateProviders/syncCookieStore/index.tsx
@@ -13,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * External dependencies.
- */
 import { useContextSelector, createContext } from 'use-context-selector';
 import React, {
   type PropsWithChildren,
