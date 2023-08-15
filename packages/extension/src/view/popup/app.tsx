63738bcfc7a110e6b8c0e8ce9088ebb51581ad54
sayedtaqui <sayedwp@gmail.com>
2023-08-15 10:06:22 +0530

    Remove ad/cookie block info

--- packages/extension/src/view/popup/app.tsx
+++ packages/extension/src/view/popup/app.tsx
@@ -148,9 +148,6 @@ const App: React.FC = () => {
         <p className="text-chart-label text-xs">
           {'Inspect cookies in the "Privacy Sandbox" panel of DevTools'}
         </p>
-        <p className="text-xxxs mt-4">
-          *Please disable ad/cookie blocking extensions for best results
-        </p>
       </div>
     </div>
   );
