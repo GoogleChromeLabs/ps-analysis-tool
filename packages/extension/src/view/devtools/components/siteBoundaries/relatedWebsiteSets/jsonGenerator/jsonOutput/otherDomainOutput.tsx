85ea45e6330ae6419b56f412a0448e18891254d9
Mayank Rana <mayankranax1@gmail.com>
2023-09-08 16:55:29 +0530

    Add Form Output component

---
+++ packages/extension/src/view/devtools/components/siteBoundaries/relatedWebsiteSets/jsonGenerator/jsonOutput/otherDomainOutput.tsx
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies.
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies.
+ */
+import type {
+  OtherWellKnownOutputType,
+  PrimaryWellKnownOutputType,
+} from '../types';
+import Output from './output';
+
+interface OtherDomainOutputProps {
+  primaryWellKnownOutput: PrimaryWellKnownOutputType | null;
+  otherWellKnownOutput: OtherWellKnownOutputType | null;
+}
+
+const OtherDomainOutput = ({
+  primaryWellKnownOutput,
+  otherWellKnownOutput,
+}: OtherDomainOutputProps) => {
+  return (
+    <div className="py-3 mb-3">
+      <div className="flex gap-2 items-center mb-4">
+        <p className="text-xs w-5 h-5 flex items-center justify-center bg-bright-navy-blue text-white rounded-full">
+          2
+        </p>
+        <p className="text-base">
+          Add the <code>.well-known</code> file to <b>all the other domains</b>{' '}
+          in your set with the following content:
+        </p>
+      </div>
+      <div id="domainsListOutput">
+        <ul className="list-disc">
+          {primaryWellKnownOutput && primaryWellKnownOutput.associatedSites && (
+            <div className="mb-2">
+              <p className="text-sm">Associated Sites</p>
+              <div className="pl-4">
+                {primaryWellKnownOutput.associatedSites.map((url) => (
+                  <li key={url}>{url}/.well-known/first-party-set.json</li>
+                ))}
+              </div>
+            </div>
+          )}
+          {primaryWellKnownOutput && primaryWellKnownOutput.serviceSites && (
+            <div className="mb-2">
+              <p className="text-sm">Service Sites</p>
+              <div className="pl-4">
+                {primaryWellKnownOutput.serviceSites.map((url) => (
+                  <li key={url}>{url}/.well-known/first-party-set.json</li>
+                ))}
+              </div>
+            </div>
+          )}
+          {primaryWellKnownOutput && primaryWellKnownOutput.ccTLDs && (
+            <div className="mb-2">
+              <p className="text-sm">Country Sites</p>
+              <div className="pl-4">
+                {Object.values(primaryWellKnownOutput.ccTLDs).map((cctlds) =>
+                  cctlds.map((cctld) => (
+                    <li key={cctld}>
+                      {cctld}/.well-known/first-party-set.json
+                    </li>
+                  ))
+                )}
+              </div>
+            </div>
+          )}
+        </ul>
+      </div>
+      <Output data={otherWellKnownOutput} />
+    </div>
+  );
+};
+
+export default OtherDomainOutput;
---
+++ packages/extension/src/view/devtools/components/siteBoundaries/relatedWebsiteSets/jsonGenerator/jsonOutput/output.tsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies.
+ */
+import React, { useState } from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
+
+/**
+ * Internal dependencies.
+ */
+import type {
+  OtherWellKnownOutputType,
+  PrimaryWellKnownOutputType,
+} from '../types';
+
+interface OutputProps {
+  data: PrimaryWellKnownOutputType | OtherWellKnownOutputType | null;
+}
+
+const Output = ({ data }: OutputProps) => {
+  const [copied, setCopied] = useState(false);
+
+  return (
+    <pre className="bg-anti-flash-white dark:bg-davys-grey p-2 rounded-md">
+      <CopyToClipboard
+        text={JSON.stringify(data, null, 2)}
+        onCopy={() => {
+          setCopied(true);
+          setTimeout(() => setCopied(false), 2000);
+        }}
+      >
+        <button className="float-right">{copied ? 'Copied!' : 'Copy'}</button>
+      </CopyToClipboard>
+      <code>{data && JSON.stringify(data, null, 2)}</code>
+    </pre>
+  );
+};
+
+export default Output;
