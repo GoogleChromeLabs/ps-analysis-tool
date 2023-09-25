6ce029ca36edfd60635dfcf5236c8ea4b291d7e3
Mayank Rana <mayankranax1@gmail.com>
2023-09-25 11:56:24 +0530

    Add custom hooks for table

---
+++ packages/extension/src/view/design-system/components/table/useColumnResizing/index.tsx
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies.
+ */
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+/**
+ * Internal dependencies.
+ */
+import type { TableColumn } from '../useTable';
+
+export type ColumnResizingOutput = {
+  columns: TableColumn[];
+  tableContainerRef: React.RefObject<HTMLDivElement>;
+  onMouseDown: (
+    selectedColumnRef: React.RefObject<HTMLTableHeaderCellElement>,
+    index: number
+  ) => void;
+};
+
+const useColumnResizing = (
+  tableColumns: TableColumn[]
+): ColumnResizingOutput => {
+  const tableContainerRef = useRef<HTMLDivElement>(null);
+  const [columns, setColumns] = useState<TableColumn[]>([]);
+
+  useEffect(() => {
+    const tableWidth =
+      tableContainerRef.current?.getBoundingClientRect().width || 0;
+
+    const newColumns = tableColumns.map((column) => {
+      return {
+        ...column,
+        width: tableWidth / tableColumns.length,
+      };
+    });
+
+    setColumns(newColumns);
+  }, [tableColumns]);
+
+  const onMouseDown = useCallback(
+    (
+      selectedColumnRef: React.RefObject<HTMLTableHeaderCellElement>,
+      index: number
+    ) => {
+      const onMove = (clientXPos: number) => {
+        const startOffset =
+          selectedColumnRef.current?.getBoundingClientRect().left || 0;
+
+        const selectedColumn = index;
+
+        const column1 = columns[selectedColumn];
+        const column2 = columns[selectedColumn + 1];
+
+        const column1Width = column1.width || 0;
+        const column2Width = column2.width || 0;
+        const widthChange = clientXPos - startOffset - column1Width;
+
+        const newColumn2Width = Math.max(20, column2Width - widthChange);
+        const newColumn1Width =
+          newColumn2Width === 20
+            ? column1Width + column2Width - 20
+            : column1Width + widthChange;
+
+        setColumns((prev) => {
+          const newColumns = [...prev];
+
+          newColumns[selectedColumn] = {
+            ...column1,
+            width: newColumn1Width,
+          };
+
+          newColumns[selectedColumn + 1] = {
+            ...column2,
+            width: newColumn2Width,
+          };
+
+          return newColumns;
+        });
+      };
+
+      const mouseEvents = {
+        moveHandler: (e: MouseEvent) => onMove(e.clientX),
+        upHandler: () => {
+          document.removeEventListener('mousemove', mouseEvents.moveHandler);
+          document.removeEventListener('mouseup', mouseEvents.upHandler);
+        },
+      };
+
+      document.addEventListener('mousemove', mouseEvents.moveHandler);
+      document.addEventListener('mouseup', mouseEvents.upHandler);
+    },
+    [columns]
+  );
+
+  return {
+    columns,
+    tableContainerRef,
+    onMouseDown,
+  };
+};
+
+export default useColumnResizing;
---
+++ packages/extension/src/view/design-system/components/table/useTable/index.tsx
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies.
+ */
+import { useMemo } from 'react';
+
+/**
+ * Internal dependencies.
+ */
+import type { CookieTableData } from '../../../../devtools/cookies.types';
+import getValueByKey from '../utils/getValueByKey';
+import useColumnSorting, {
+  type ColumnSortingOutput,
+} from '../useColumnSorting';
+import useColumnVisibility, {
+  type ColumnVisibilityOutput,
+} from '../useColumnVisibility';
+import useColumnResizing, {
+  type ColumnResizingOutput,
+} from '../useColumnResizing';
+
+export type TableData = CookieTableData;
+
+export type InfoType = number | string | boolean;
+
+export type TableColumn = {
+  header: string;
+  accessorKey: string;
+  cell?: (info: InfoType) => React.JSX.Element | InfoType;
+  enableHiding?: boolean;
+  width?: number;
+};
+
+export type TableRow = {
+  [accessorKey: string]: {
+    value: React.JSX.Element | InfoType;
+    originalData: TableData;
+  };
+};
+
+export type TableOutput = {
+  columns: TableColumn[];
+  hideableColumns: TableColumn[];
+  rows: TableRow[];
+  sortKey: ColumnSortingOutput['sortKey'];
+  sortOrder: ColumnSortingOutput['sortOrder'];
+  setSortKey: ColumnSortingOutput['setSortKey'];
+  setSortOrder: ColumnSortingOutput['setSortOrder'];
+  hideColumn: ColumnVisibilityOutput['hideColumn'];
+  toggleVisibility: ColumnVisibilityOutput['toggleVisibility'];
+  areAllColumnsVisible: ColumnVisibilityOutput['areAllColumnsVisible'];
+  showColumn: ColumnVisibilityOutput['showColumn'];
+  isColumnHidden: ColumnVisibilityOutput['isColumnHidden'];
+  tableContainerRef: ColumnResizingOutput['tableContainerRef'];
+  onMouseDown: ColumnResizingOutput['onMouseDown'];
+};
+
+interface useTableProps {
+  tableColumns: TableColumn[];
+  data: TableData[];
+}
+
+const useTable = ({ tableColumns, data }: useTableProps): TableOutput => {
+  const {
+    visibleColumns,
+    hideColumn,
+    toggleVisibility,
+    areAllColumnsVisible,
+    showColumn,
+    isColumnHidden,
+  } = useColumnVisibility(tableColumns);
+
+  const { columns, tableContainerRef, onMouseDown } =
+    useColumnResizing(visibleColumns);
+
+  const { sortedData, sortKey, sortOrder, setSortKey, setSortOrder } =
+    useColumnSorting(data);
+
+  const rows = useMemo(() => {
+    return sortedData.map((_data) => {
+      const row = {} as TableRow;
+
+      columns.forEach((column) => {
+        const value = getValueByKey(column.accessorKey, _data);
+        row[column.accessorKey] = {
+          value: column.cell?.(value) ?? value,
+          originalData: _data,
+        };
+      });
+
+      return row;
+    });
+  }, [sortedData, columns]);
+
+  const hideableColumns = useMemo(
+    () => tableColumns.filter((column) => column.enableHiding !== false),
+    [tableColumns]
+  );
+
+  return {
+    columns,
+    hideableColumns,
+    rows,
+    sortKey,
+    sortOrder,
+    setSortKey,
+    setSortOrder,
+    hideColumn,
+    toggleVisibility,
+    areAllColumnsVisible,
+    showColumn,
+    isColumnHidden,
+    tableContainerRef,
+    onMouseDown,
+  };
+};
+
+export default useTable;
