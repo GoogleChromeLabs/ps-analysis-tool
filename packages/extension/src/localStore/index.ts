79fc02c9f5688b45a5c167a86f185daffbcffb32
sayedtaqui <sayedwp@gmail.com>
2023-10-04 10:14:18 +0530

    Add missing default as

--- packages/extension/src/localStore/index.ts
+++ packages/extension/src/localStore/index.ts
@@ -13,6 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export { CookieStore } from './cookieStore';
-export { PreferenceStore } from './preferenceStore';
+export { default as CookieStore } from './cookieStore';
+export { default as PreferenceStore } from './preferenceStore';
 export * from './types';
