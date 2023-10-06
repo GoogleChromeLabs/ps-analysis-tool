18bd4c762a8427931b47ab07d693570c00988054
Mayank Rana <mayankranax1@gmail.com>
2023-10-06 18:09:22 +0530

    Change size of column and table container

--- packages/design-system/src/components/table/useColumnResizing/index.tsx
+++ packages/design-system/src/components/table/useColumnResizing/index.tsx
@@ -100,20 +100,20 @@ const useColumnResizing = (
           column2Width = column2.width || 0;
         const widthChange = clientXPos - (startOffset + column1Width);
 
-        let newColumn1Width = 20,
-          newColumn2Width = 20;
+        let newColumn1Width = 40,
+          newColumn2Width = 40;
 
         if (widthChange > 0) {
-          newColumn2Width = Math.max(20, column2Width - widthChange);
+          newColumn2Width = Math.max(40, column2Width - widthChange);
           newColumn1Width =
-            newColumn2Width === 20
-              ? column1Width + column2Width - 20
+            newColumn2Width === 40
+              ? column1Width + column2Width - 40
               : column1Width + widthChange;
         } else {
-          newColumn1Width = Math.max(20, column1Width + widthChange);
+          newColumn1Width = Math.max(40, column1Width + widthChange);
           newColumn2Width =
-            newColumn1Width === 20
-              ? column1Width + column2Width - 20
+            newColumn1Width === 40
+              ? column1Width + column2Width - 40
               : column2Width - widthChange;
         }
 
