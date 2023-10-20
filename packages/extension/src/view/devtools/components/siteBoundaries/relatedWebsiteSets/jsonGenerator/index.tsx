4711ef4764da79bb61935b029c8e9c8438df5c3a
Mayank Rana <mayankranax1@gmail.com>
2023-10-20 11:40:51 +0530

    Keep json generator open by default

--- packages/extension/src/view/devtools/components/siteBoundaries/relatedWebsiteSets/jsonGenerator/index.tsx
+++ packages/extension/src/view/devtools/components/siteBoundaries/relatedWebsiteSets/jsonGenerator/index.tsx
@@ -17,8 +17,8 @@
 /**
  * External dependencies.
  */
-import React, { useEffect, useRef } from 'react';
-import { Button, Cross } from '@ps-analysis-tool/design-system';
+import React from 'react';
+import { Button } from '@ps-analysis-tool/design-system';
 
 /**
  * Internal dependencies.
@@ -31,12 +31,7 @@ import CountrySites from './countrySites';
 import JsonOutput from './jsonOutput';
 import useGeneratorForm from './useGeneratorForm';
 
-interface RWSJsonGeneratorProps {
-  open: boolean;
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-const RWSJsonGenerator = ({ open, setOpen }: RWSJsonGeneratorProps) => {
+const RWSJsonGenerator = () => {
   const {
     state: {
       contact,
@@ -66,123 +61,99 @@ const RWSJsonGenerator = ({ open, setOpen }: RWSJsonGeneratorProps) => {
     },
   } = useGeneratorForm();
 
-  const formContainer = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (!open) {
-      return;
-    }
-
-    if (formContainer.current) {
-      formContainer.current.scrollIntoView({
-        behavior: 'smooth',
-      });
-    }
-  }, [open]);
-
   return (
     <>
-      {open && (
-        <div className="overflow-auto px-4" ref={formContainer}>
-          <div className="text-raisin-black dark:text-bright-gray w-full min-w-[33rem]">
-            <h1 className="text-lg font-semibold mt-4">
-              Related Website Sets JSON Generator
-            </h1>
-            <p className="text-xs py-3">
-              This tool generates the JSON resources needed to make a Related
-              Website Sets(RWS) submission. It <b>does not</b> perform all the
-              required technical validations (see full requirements{' '}
-              <a
-                className="text-bright-navy-blue dark:text-jordy-blue hover:opacity-80"
-                title="https://github.com/GoogleChrome/related-website-sets/blob/main/RWS-Submission_Guidelines.md"
-                href="https://github.com/GoogleChrome/related-website-sets/blob/main/RWS-Submission_Guidelines.md"
-                target="_blank"
-                rel="noreferrer"
-              >
-                here
-              </a>
-              ).
+      <div className="overflow-auto py-6">
+        <div className="text-raisin-black dark:text-bright-gray w-full min-w-[33rem]">
+          <h1 className="text-lg font-semibold">
+            Related Website Sets JSON Generator
+          </h1>
+          <p className="text-xs py-3">
+            This tool generates the JSON resources needed to make a Related
+            Website Sets(RWS) submission. It <b>does not</b> perform all the
+            required technical validations (see full requirements{' '}
+            <a
+              className="text-bright-navy-blue dark:text-jordy-blue hover:opacity-80"
+              title="https://github.com/GoogleChrome/related-website-sets/blob/main/RWS-Submission_Guidelines.md"
+              href="https://github.com/GoogleChrome/related-website-sets/blob/main/RWS-Submission_Guidelines.md"
+              target="_blank"
+              rel="noreferrer"
+            >
+              here
+            </a>
+            ).
+          </p>
+          <div className="mt-4 bg-anti-flash-white dark:bg-charleston-green border rounded-xl border-gray-200 dark:border-quartz px-4 py-3 shadow">
+            <p className="text-base my-3">
+              Enter your Related Website Sets details below:
             </p>
-            <div className="my-4 bg-anti-flash-white dark:bg-charleston-green border rounded-xl border-gray-200 dark:border-quartz px-4 py-3 shadow">
-              <button
-                onClick={() => {
-                  setOpen(false);
-                }}
-                className="float-right pt-1"
-              >
-                <Cross />
-              </button>
-              <p className="text-base my-3">
-                Enter your Related Website Sets details below:
-              </p>
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  submitForm();
-                }}
-                onReset={resetForm}
-              >
-                <ContactEmail
-                  contact={contact}
-                  setContact={setContact}
-                  validationFailed={!validationPassed}
-                />
-                <PrimaryDomain
-                  primaryDomain={primaryDomain}
-                  setPrimaryDomain={setPrimaryDomain}
-                  validationFailed={!validationPassed}
-                />
-                <div className="divide-y divide-american-silver dark:divide-quartz">
-                  <AssociatedSites
-                    associatedSites={associatedSites}
-                    setAssociatedSites={setAssociatedSites}
-                    removeAssociatedSite={removeAssociatedSite}
-                    addAssociatedSite={addAssociatedSite}
-                    validationFailed={!validationPassed}
-                  />
-                  <ServiceSites
-                    serviceSites={serviceSites}
-                    setServiceSites={setServiceSites}
-                    removeServiceSite={removeServiceSite}
-                    addServiceSite={addServiceSite}
-                    validationFailed={!validationPassed}
-                  />
-                  <CountrySites
-                    countrySites={countrySites}
-                    setCountrySites={setCountrySites}
-                    removeCountrySite={removeCountrySite}
-                    addCountrySite={addCountrySite}
-                    availableSites={[
-                      primaryDomain.url,
-                      ...associatedSites.map(({ url }) => url),
-                      ...serviceSites.map(({ url }) => url),
-                    ].filter((url) => Boolean(url))}
-                    validationFailed={!validationPassed}
-                  />
-                </div>
-                <div className="my-3 flex items-center justify-start">
-                  {loading ? (
-                    <div className="flex items-center justify-center">
-                      <div className="w-6 h-6 rounded-full animate-spin border-t-transparent border-solid border-blue-700 border-2" />
-                    </div>
-                  ) : (
-                    <div className="flex gap-2">
-                      <Button text="Generate" type="submit" />
-                      <Button text="Reset" type="reset" variant="secondary" />
-                    </div>
-                  )}
-                </div>
-              </form>
-            </div>
-            {!loading && validationPassed && (
-              <JsonOutput
-                primaryWellKnownOutput={primaryWellKnownOutput}
-                otherWellKnownOutput={otherWellKnownOutput}
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                submitForm();
+              }}
+              onReset={resetForm}
+            >
+              <ContactEmail
+                contact={contact}
+                setContact={setContact}
+                validationFailed={!validationPassed}
               />
-            )}
+              <PrimaryDomain
+                primaryDomain={primaryDomain}
+                setPrimaryDomain={setPrimaryDomain}
+                validationFailed={!validationPassed}
+              />
+              <div className="divide-y divide-american-silver dark:divide-quartz">
+                <AssociatedSites
+                  associatedSites={associatedSites}
+                  setAssociatedSites={setAssociatedSites}
+                  removeAssociatedSite={removeAssociatedSite}
+                  addAssociatedSite={addAssociatedSite}
+                  validationFailed={!validationPassed}
+                />
+                <ServiceSites
+                  serviceSites={serviceSites}
+                  setServiceSites={setServiceSites}
+                  removeServiceSite={removeServiceSite}
+                  addServiceSite={addServiceSite}
+                  validationFailed={!validationPassed}
+                />
+                <CountrySites
+                  countrySites={countrySites}
+                  setCountrySites={setCountrySites}
+                  removeCountrySite={removeCountrySite}
+                  addCountrySite={addCountrySite}
+                  availableSites={[
+                    primaryDomain.url,
+                    ...associatedSites.map(({ url }) => url),
+                    ...serviceSites.map(({ url }) => url),
+                  ].filter((url) => Boolean(url))}
+                  validationFailed={!validationPassed}
+                />
+              </div>
+              <div className="my-3 flex items-center justify-end">
+                {loading ? (
+                  <div className="flex items-center justify-center">
+                    <div className="w-6 h-6 rounded-full animate-spin border-t-transparent border-solid border-blue-700 border-2" />
+                  </div>
+                ) : (
+                  <div className="flex gap-2">
+                    <Button text="Generate" type="submit" />
+                    <Button text="Reset" type="reset" variant="secondary" />
+                  </div>
+                )}
+              </div>
+            </form>
           </div>
+          {!loading && validationPassed && (
+            <JsonOutput
+              primaryWellKnownOutput={primaryWellKnownOutput}
+              otherWellKnownOutput={otherWellKnownOutput}
+            />
+          )}
         </div>
-      )}
+      </div>
     </>
   );
 };
