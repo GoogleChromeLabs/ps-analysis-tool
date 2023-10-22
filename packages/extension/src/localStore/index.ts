fb2d6ebe7ff38a84bb777aa023e9bb54356e3e24
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2023-10-22 12:29:20 +0530

    Remove unnecessary code diff.

--- packages/extension/src/localStore/index.ts
+++ packages/extension/src/localStore/index.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export * from './types';
+
 export { default as CookieStore } from './cookieStore';
 export { default as PreferenceStore } from './preferenceStore';
+export * from './types';
