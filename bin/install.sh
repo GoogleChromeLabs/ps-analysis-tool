6affba2abf187803ca99998db4f58261ed50eff6
Mitesh Shah <Mr.Miteshah@gmail.com>
2023-10-25 15:53:14 +0530

    Update Installation Script URL for Latest Version

--- bin/install.sh
+++ bin/install.sh
@@ -4,7 +4,7 @@
 mkdir -p ~/bin/
 
 # Download Chrome Launcher Script and save to ~/bin/chrome_launcher.sh
-curl -s https://raw.githubusercontent.com/GoogleChromeLabs/ps-analysis-tool/4d26f3daeea86db82f1e39692e23d79713627d72/chrome_launcher.sh -o ~/bin/chrome_launcher.sh
+curl -s https://raw.githubusercontent.com/GoogleChromeLabs/ps-analysis-tool/01c9bd6d0823e4650e12395538f7f30ff3453def/bin/chrome_launcher.sh -o ~/bin/chrome_launcher.sh
 
 chmod 755 ~/bin/chrome_launcher.sh
 
