08f63574f26f10ab3168c738a6e9477eb463a457
Mitesh Shah <Mr.Miteshah@gmail.com>
2023-10-25 10:58:09 +0530

    Added script from /usr/local/bin to ~/bin so wont need sudo

--- install.sh
+++ install.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-# Download Chrome Launcher Script and save to /usr/local/bin/chrome_launcher.sh
-curl -s https://raw.githubusercontent.com/GoogleChromeLabs/ps-analysis-tool/4d26f3daeea86db82f1e39692e23d79713627d72/chrome_launcher.sh -o /usr/local/bin/chrome_launcher.sh
+# create bin directory inside user home
+mkdir -p ~/bin/
+
+# Download Chrome Launcher Script and save to ~/bin/chrome_launcher.sh
+curl -s https://raw.githubusercontent.com/GoogleChromeLabs/ps-analysis-tool/4d26f3daeea86db82f1e39692e23d79713627d72/chrome_launcher.sh -o ~/bin/chrome_launcher.sh
+
+chmod 755 ~/bin/chrome_launcher.sh
 
 # Check Shell and auto source chrome_launcher.sh
 case $SHELL in
 */zsh)
   grep "chrome_launcher.sh" ~/.zshrc
   if [ $? -ne 0 ]; then
-    echo "source /usr/local/bin/chrome_launcher.sh" >> ~/.zshrc
+    echo "if [ -f ~/bin/chrome_launcher.sh ]; then" >> ~/.zshrc
+      echo -e "\tsource ~/bin/chrome_launcher.sh" >> ~/.zshrc
+    echo "fi" >> ~/.zshrc
   fi
    ;;
 */bash)
   grep "chrome_launcher.sh" ~/.bashrc
   if [ $? -ne 0 ]; then
-    echo "source /usr/local/bin/chrome_launcher.sh" >> ~/.bashrc
+    echo "if [ -f ~/bin/chrome_launcher.sh ]; then" >> ~/.bashrc
+      echo -e "\tsource ~/bin/chrome_launcher.sh" >> ~/.bashrc
+    echo "fi" >> ~/.bashrc
   fi
 ;;
 esac
 
 # Source into current terminal session as well
-source /usr/local/bin/chrome_launcher.sh
+source ~/bin/chrome_launcher.sh
