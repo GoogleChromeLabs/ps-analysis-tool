01c9bd6d0823e4650e12395538f7f30ff3453def
Mitesh Shah <Mr.Miteshah@gmail.com>
2023-10-25 15:51:47 +0530

    Update Extenstion Version and Script Location

--- bin/chrome_launcher.sh
+++ bin/chrome_launcher.sh
@@ -2,7 +2,7 @@
 
 # Download Extension
 extension_setup() {
-  ps_analysis_tool_version=v0.3.0
+  ps_analysis_tool_version=v0.3.1
   cd /tmp/
   if [ ! -d /tmp/ps-analysis-tool-$ps_analysis_tool_version ]; then
     mkdir -p /tmp/ps-analysis-tool-$ps_analysis_tool_version
