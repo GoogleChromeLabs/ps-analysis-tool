3e4820f81ff2e7af3e6caf5aba93e7e02cfd80b3
Mitesh Shah <Mr.Miteshah@gmail.com>
2023-10-31 11:26:03 +0530

    Update File Location from branch instead of Commit

--- bin/install.sh
+++ bin/install.sh
@@ -4,7 +4,7 @@
 mkdir -p ~/bin/
 
 # Download Chrome Launcher Script and save to ~/bin/chrome_launcher.sh
-curl -s https://raw.githubusercontent.com/GoogleChromeLabs/ps-analysis-tool/976539048a218b2dde07ed0279ed35a3c3567620/bin/chrome_launcher.sh -o ~/bin/chrome_launcher.sh
+curl -s https://raw.githubusercontent.com/GoogleChromeLabs/ps-analysis-tool/Setup-Different-Chrome-For-Demo/bin/chrome_launcher.sh -o ~/bin/chrome_launcher.sh
 
 chmod 755 ~/bin/chrome_launcher.sh
 
