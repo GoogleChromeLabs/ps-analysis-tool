030d08669f6d299d0b9dc9b994a40c3f5167d77a
Mayank Rana <58820001+mayan-000@users.noreply.github.com>
2023-11-30 17:21:56 +0530

    Refactor: Move dashboard's data processing to utils (#192)
    * Move data processing to utils
    
    * Move cookie reshaping to utils
    
    * Update icons
    
    * Setup input textbox and use in cookie top bar
    
    * Move extractCookies into separate file
    
    * Update Input -> TextInput
    
    * Add test cases
    
    * Change function param name
    
    * Remoce jest-dom
    
    * Change TextInput -> SearchInput
    
    * fix:attribute cookie of frame about:blank to unknown frames
    
    * Update unknown frame with constant
    
    * fix: frame cookies parsing loop
    
    * Make frameurl string array for dashboard
    
    * Fix test
    
    * Update frameUrl to frameUrls
    
    ---------
    
    Co-authored-by: ayushnirwal <coding.carrots@gmail.com>

--- packages/cli-dashboard/src/components/siteReport/components/layout.tsx
+++ packages/cli-dashboard/src/components/siteReport/components/layout.tsx
@@ -36,7 +36,11 @@ const Layout = ({ selectedSite }: LayoutProps) => {
     frameUrls: [
       ...new Set(
         Object.values(state.tabCookies)
-          .map((cookie) => cookie.frameUrl)
+          .reduce((acc, cookie) => {
+            acc.push(...(cookie.frameUrls as string[]));
+
+            return acc;
+          }, [] as string[])
           .filter(
             (url) => url?.includes('http') || url === UNKNOWN_FRAME_KEY
           ) as string[]
--- packages/cli-dashboard/src/components/siteReport/tabs/cookies/index.tsx
+++ packages/cli-dashboard/src/components/siteReport/tabs/cookies/index.tsx
@@ -38,12 +38,11 @@ const CookiesTab = ({ selectedFrameUrl, selectedSite }: CookiesTabProps) => {
   const tabFrames = useMemo<TabFrames>(
     () =>
       Object.values(tabCookies).reduce((acc, cookie) => {
-        if (
-          cookie.frameUrl?.includes('http') ||
-          cookie.frameUrl === UNKNOWN_FRAME_KEY
-        ) {
-          acc[cookie.frameUrl] = {} as TabFrames[string];
-        }
+        (cookie.frameUrls as string[]).forEach((url) => {
+          if (url?.includes('http') || url === UNKNOWN_FRAME_KEY) {
+            acc[url] = {} as TabFrames[string];
+          }
+        });
         return acc;
       }, {} as TabFrames),
     [tabCookies]
