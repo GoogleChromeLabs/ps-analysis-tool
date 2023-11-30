bf269e23edca1f40e356f56824862bdb22f07a5f
Mayank Rana <mayankranax1@gmail.com>
2023-11-30 10:48:55 +0530

    Create columns, filters and serach for table

--- packages/extension/src/view/devtools/components/cookies/cookiesListing/index.tsx
+++ packages/extension/src/view/devtools/components/cookies/cookiesListing/index.tsx
@@ -16,88 +16,376 @@
 /**
  * External dependencies.
  */
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Resizable } from 're-resizable';
 import type { CookieTableData } from '@ps-analysis-tool/common';
-import { CookieDetails } from '@ps-analysis-tool/design-system';
-import CookieTableContainer from './cookieTableContainer';
+import {
+  CookieDetails,
+  CookieTable,
+  RefreshButton,
+  type InfoType,
+  type TableColumn,
+  type TableFilter,
+} from '@ps-analysis-tool/design-system';
 
 /**
  * Internal dependencies.
  */
 import { useCookieStore } from '../../../stateProviders/syncCookieStore';
-import { useFilterManagementStore } from '../../../stateProviders/filterManagementStore';
-import ChipsBar from '../cookieFilter/chips';
-import CookieTopBar from '../cookieTopBar';
-import FiltersList from '../cookieFilter';
 
-const CookiesListing = () => {
-  const { selectedFrame } = useCookieStore(({ state }) => ({
-    selectedFrame: state.selectedFrame,
-  }));
+interface CookiesListingProps {
+  setFilteredCookies: React.Dispatch<CookieTableData[]>;
+}
 
-  const { filteredCookies, cookiesAvailable } = useFilterManagementStore(
-    ({ state }) => ({
-      filteredCookies: state.filteredCookies,
-      cookiesAvailable: state.cookiesAvailable,
-    })
-  );
+const CookiesListing = ({ setFilteredCookies }: CookiesListingProps) => {
+  const { selectedFrame, cookies, tabFrames, getCookiesSetByJavascript } =
+    useCookieStore(({ state, actions }) => ({
+      selectedFrame: state.selectedFrame,
+      cookies: state.tabCookies || {},
+      tabFrames: state.tabFrames,
+      getCookiesSetByJavascript: actions.getCookiesSetByJavascript,
+    }));
 
-  const [isFilterMenuOpen, setIsFilterMenuOpen] = useState<boolean>(false);
+  const frameFilteredCookies = useMemo(() => {
+    const _frameFilteredCookies: { [key: string]: CookieTableData } = {};
+    if (cookies && selectedFrame && tabFrames && tabFrames[selectedFrame]) {
+      Object.entries(cookies).forEach(([key, cookie]) => {
+        tabFrames[selectedFrame].frameIds?.forEach((frameId) => {
+          if (cookie.frameIdList?.includes(frameId)) {
+            _frameFilteredCookies[key] = cookie;
+          }
+        });
+      });
+    }
+    return _frameFilteredCookies;
+  }, [cookies, selectedFrame, tabFrames]);
+
+  useEffect(() => {
+    setFilteredCookies(Object.values(frameFilteredCookies));
+  }, [frameFilteredCookies, setFilteredCookies]);
 
   const [selectedFrameCookie, setSelectedFrameCookie] = useState<{
     [frame: string]: CookieTableData | null;
   } | null>(null);
 
-  const toggleFilterMenu = () => {
-    setIsFilterMenuOpen((p) => !p);
-  };
+  const tableColumns = useMemo<TableColumn[]>(
+    () => [
+      {
+        header: 'Name',
+        accessorKey: 'parsedCookie.name',
+        cell: (info: InfoType) => info,
+        enableHiding: false,
+      },
+      {
+        header: 'Value',
+        accessorKey: 'parsedCookie.value',
+        cell: (info: InfoType) => info,
+      },
+      {
+        header: 'Domain',
+        accessorKey: 'parsedCookie.domain',
+        cell: (info: InfoType) => info,
+      },
+      {
+        header: 'Path',
+        accessorKey: 'parsedCookie.path',
+        cell: (info: InfoType) => info,
+      },
+      {
+        header: 'Expires / Max-Age',
+        accessorKey: 'parsedCookie.expires',
+        cell: (info: InfoType) => (info ? info : 'Session'),
+      },
+      {
+        header: 'HttpOnly',
+        accessorKey: 'parsedCookie.httponly',
+        cell: (info: InfoType) => (
+          <p className="flex justify-center items-center">
+            {info ? <span className="font-serif">✓</span> : ''}
+          </p>
+        ),
+      },
+      {
+        header: 'SameSite',
+        accessorKey: 'parsedCookie.samesite',
+        cell: (info: InfoType) => <span className="capitalize">{info}</span>,
+      },
+      {
+        header: 'Secure',
+        accessorKey: 'parsedCookie.secure',
+        cell: (info: InfoType) => (
+          <p className="flex justify-center items-center">
+            {info ? <span className="font-serif">✓</span> : ''}
+          </p>
+        ),
+      },
+      {
+        header: 'Category',
+        accessorKey: 'analytics.category',
+        cell: (info: InfoType) => info,
+      },
+      {
+        header: 'Platform',
+        accessorKey: 'analytics.platform',
+        cell: (info: InfoType) => info,
+      },
+      {
+        header: 'Scope',
+        accessorKey: 'isFirstParty',
+        cell: (info: InfoType) => (
+          <p className="truncate w-full">
+            {!info ? 'Third Party' : 'First Party'}
+          </p>
+        ),
+      },
+      {
+        header: 'Cookie Accepted',
+        accessorKey: 'isCookieSet',
+        cell: (info: InfoType) => (
+          <p className="flex justify-center items-center">
+            {info ? (
+              <span className="font-serif">✓</span>
+            ) : (
+              <span className="font-serif">✗</span>
+            )}
+          </p>
+        ),
+      },
+      {
+        header: 'GDPR Portal',
+        accessorKey: 'analytics.gdprUrl',
+        cell: (info: InfoType) => (
+          <a
+            className="text-blue-500 hover:underline"
+            target="_blank"
+            href={info as string}
+            rel="noreferrer"
+          >
+            <abbr title={info as string}>{info}</abbr>
+          </a>
+        ),
+      },
+    ],
+    []
+  );
+
+  const filters = useMemo<TableFilter>(
+    () => ({
+      'analytics.category': {
+        title: 'Category',
+      },
+      isFirstParty: {
+        title: 'Scope',
+        hasStaticFilterValues: true,
+        filterValues: {
+          'First Party': {
+            selected: false,
+          },
+          'Third Party': {
+            selected: false,
+          },
+        },
+        comparator: (value: InfoType, filterValue: string) => {
+          const val = value as boolean;
+          return val === (filterValue === 'First Party');
+        },
+      },
+      'parsedCookie.domain': {
+        title: 'Domain',
+      },
+      'parsedCookie.httponly': {
+        title: 'HttpOnly',
+        hasStaticFilterValues: true,
+        filterValues: {
+          True: {
+            selected: false,
+          },
+          False: {
+            selected: false,
+          },
+        },
+        comparator: (value: InfoType, filterValue: string) => {
+          const val = value as boolean;
+          return val === (filterValue === 'True');
+        },
+      },
+      'parsedCookie.samesite': {
+        title: 'SameSite',
+        hasStaticFilterValues: true,
+        filterValues: {
+          None: {
+            selected: false,
+          },
+          Lax: {
+            selected: false,
+          },
+          Strict: {
+            selected: false,
+          },
+        },
+        comparator: (value: InfoType, filterValue: string) => {
+          const val = value as string;
+          return val?.toLowerCase() === filterValue.toLowerCase();
+        },
+      },
+      'parsedCookie.secure': {
+        title: 'Secure',
+        hasStaticFilterValues: true,
+        filterValues: {
+          True: {
+            selected: false,
+          },
+          False: {
+            selected: false,
+          },
+        },
+        comparator: (value: InfoType, filterValue: string) => {
+          const val = value as boolean;
+          return val === (filterValue === 'True');
+        },
+      },
+      'parsedCookie.path': {
+        title: 'Path',
+      },
+      'parsedCookie.expires': {
+        title: 'Retention Period',
+        hasStaticFilterValues: true,
+        filterValues: {
+          Session: {
+            selected: false,
+          },
+          'Short Term (< 24h)': {
+            selected: false,
+          },
+          'Medium Term (24h - 1 week)': {
+            selected: false,
+          },
+          'Long Term (1 week - 1 month)': {
+            selected: false,
+          },
+          'Extended Term (> 1 month)': {
+            selected: false,
+          },
+        },
+        comparator: (value: InfoType, filterValue: string) => {
+          let diff = 0;
+          const val = value as string;
+          switch (filterValue) {
+            case 'Session':
+              return val === 'Session';
+
+            case 'Short Term (< 24h)':
+              diff = Date.parse(val) - Date.now();
+              return diff < 86400000;
+
+            case 'Medium Term (24h - 1 week)':
+              diff = Date.parse(val) - Date.now();
+              return diff >= 86400000 && diff < 604800000;
+
+            case 'Long Term (1 week - 1 month)':
+              diff = Date.parse(val) - Date.now();
+              return diff >= 604800000 && diff < 2629743833;
+
+            case 'Extended Term (> 1 month)':
+              diff = Date.parse(val) - Date.now();
+              return diff >= 2629743833;
+
+            default:
+              return false;
+          }
+        },
+      },
+      'analytics.platform': {
+        title: 'Platform',
+      },
+      isCookieSet: {
+        title: 'Cookie Accepted',
+        description:
+          "Whether the cookie was accepted(set) in Chrome's Cookie Store",
+        hasStaticFilterValues: true,
+        filterValues: {
+          True: {
+            selected: false,
+          },
+          False: {
+            selected: false,
+          },
+        },
+        comparator: (value: InfoType, filterValue: string) => {
+          const val = !value;
+          return val === (filterValue === 'True');
+        },
+      },
+      headerType: {
+        title: 'Set Via',
+        hasStaticFilterValues: true,
+        filterValues: {
+          HTTP: {
+            selected: false,
+          },
+          JS: {
+            selected: false,
+          },
+        },
+        comparator: (value: InfoType, filterValue: string) => {
+          const val = value as string;
+          const isJS = val === 'JS';
+
+          return isJS === (filterValue === 'JS');
+        },
+      },
+    }),
+    []
+  );
+
+  const searchKeys = useMemo<string[]>(
+    () => ['parsedCookie.name', 'parsedCookie.domain'],
+    []
+  );
+
+  const tablePersistentSettingsKey = useMemo(() => {
+    if (!selectedFrame) {
+      return 'cookieListing';
+    }
+
+    return `cookieListing#${selectedFrame}`;
+  }, [selectedFrame]);
+
+  const extraInterfaceToTopBar = useMemo(() => {
+    return <RefreshButton onClick={getCookiesSetByJavascript} />;
+  }, [getCookiesSetByJavascript]);
 
   return (
     <div className="w-full h-full flex flex-col">
-      <CookieTopBar
-        cookiesAvailable={cookiesAvailable}
-        isFilterMenuOpen={isFilterMenuOpen}
-        toggleFilterMenu={toggleFilterMenu}
-        filteredCookies={filteredCookies}
-      />
-      {cookiesAvailable && <ChipsBar />}
-      <div className="w-full flex-1 overflow-hidden h-full flex flex-col">
-        <Resizable
-          defaultSize={{
-            width: '100%',
-            height: '80%',
-          }}
-          minHeight="6%"
-          maxHeight="95%"
-          enable={{
-            top: false,
-            right: false,
-            bottom: true,
-            left: false,
-          }}
-          className="flex"
-        >
-          {cookiesAvailable && isFilterMenuOpen && (
-            <Resizable
-              minWidth="100px"
-              maxWidth="50%"
-              enable={{
-                right: true,
-              }}
-            >
-              <FiltersList />
-            </Resizable>
-          )}
-          <CookieTableContainer
-            cookies={filteredCookies}
-            selectedFrame={selectedFrame}
-            selectedFrameCookie={selectedFrameCookie}
-            setSelectedFrameCookie={setSelectedFrameCookie}
-          />
-        </Resizable>
-        <CookieDetails selectedFrameCookie={selectedFrameCookie} />
-      </div>
+      <Resizable
+        defaultSize={{
+          width: '100%',
+          height: '80%',
+        }}
+        minHeight="6%"
+        maxHeight="95%"
+        enable={{
+          top: false,
+          right: false,
+          bottom: true,
+          left: false,
+        }}
+        className="h-full flex"
+      >
+        <CookieTable
+          data={Object.values(frameFilteredCookies)}
+          tableColumns={tableColumns}
+          showTopBar={true}
+          tableFilters={filters}
+          tableSearchKeys={searchKeys}
+          tablePersistentSettingsKey={tablePersistentSettingsKey}
+          selectedFrame={selectedFrame}
+          selectedFrameCookie={selectedFrameCookie}
+          setSelectedFrameCookie={setSelectedFrameCookie}
+          extraInterfaceToTopBar={extraInterfaceToTopBar}
+        />
+      </Resizable>
+      <CookieDetails selectedFrameCookie={selectedFrameCookie} />
     </div>
   );
 };
