92aa47278c47e28dc6e3a315066f8ce277b376e3
ayushnirwal <coding.carrots@gmail.com>
2023-12-04 12:06:08 +0530

    refactor:report generation in cli-dashboard

---
+++ packages/cli-dashboard/src/components/utils/generateReports/generateAllCookiesCSV.ts
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import type { CompleteJson, CookieJsonDataType } from '../../../types';
+
+export const cookieDataHeader = [
+  'Name',
+  'Value',
+  'Domain',
+  'Path',
+  'Expires',
+  'Http Only',
+  'Scope',
+  'Secure',
+  'Same Site',
+  'Platform',
+  'Category',
+  'Cookie Affected',
+  'GDPRPortal',
+];
+
+const generateAllCookiesCSV = (siteAnalysisData: CompleteJson): string => {
+  const frameCookieDataMap = siteAnalysisData.cookieData;
+
+  const cookieMap: Map<string, CookieJsonDataType> = new Map();
+
+  // More than one frame can use one cookie, need to make a map for gettig unique entries.
+  Object.entries(frameCookieDataMap).forEach(([, { frameCookies }]) => {
+    Object.entries(frameCookies).forEach(([cookieKey, cookieData]) => {
+      cookieMap.set(cookieKey, cookieData);
+    });
+  });
+
+  let cookieRecords = '';
+
+  for (const cookie of cookieMap.values()) {
+    //@TODO sanitize array elements
+    //This should be in the same order as cookieDataHeader
+    const recordsArray = [
+      cookie.name,
+      cookie.value,
+      cookie.domain,
+      cookie.path,
+      cookie.expires,
+      cookie.httpOnly,
+      cookie.isFirstParty ? 'First Party' : 'Third Party',
+      cookie.secure,
+      cookie.sameSite,
+      cookie.platform,
+      cookie.category,
+      cookie.isBlocked,
+      cookie.GDPR || 'NA',
+    ];
+
+    cookieRecords += recordsArray.join(',') + '\r\n';
+  }
+
+  return cookieDataHeader.join(',') + '\r\n' + cookieRecords;
+};
+
+export default generateAllCookiesCSV;
---
+++ packages/cli-dashboard/src/components/utils/generateReports/generateTechnologyCSV.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import type { CompleteJson } from '../../../types';
+
+const technologyDataHeader = [
+  'Name',
+  'Description',
+  'Confidence',
+  'Website',
+  'Categories',
+];
+
+const generateTechnologyCSV = (siteAnalysisData: CompleteJson): string => {
+  const technologyDataArray = siteAnalysisData.technologyData;
+
+  let technologyRecords = '';
+
+  for (const technologyData of technologyDataArray) {
+    //@TODO sanitize array elements
+    //This should be in the same order as technologyDataHeader
+
+    const categoryRecord = technologyData.categories.reduce(
+      (acc, { name }) => (acc += `${name}|`),
+      ''
+    );
+
+    const recordsArray = [
+      technologyData.name,
+      technologyData.description,
+      technologyData.confidence,
+      technologyData.website,
+      categoryRecord.slice(0, -1),
+    ];
+
+    technologyRecords += recordsArray.join(',') + '\r\n';
+  }
+
+  return technologyDataHeader.join(',') + '\r\n' + technologyRecords;
+};
+
+export default generateTechnologyCSV;
