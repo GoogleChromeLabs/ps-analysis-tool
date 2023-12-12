264d2cf2d77054c6e81eff063bbd5dcb01899e30
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2023-12-12 12:49:39 +0530

    Fix column sorting.

--- packages/design-system/src/components/table/useColumnSorting/index.tsx
+++ packages/design-system/src/components/table/useColumnSorting/index.tsx
@@ -91,13 +91,8 @@ const useColumnSorting = (
 
   const sortedData = useMemo(() => {
     const _sortedData = [...data].sort((a, b) => {
-      let candidateA = getValueByKey(sortKey, a);
-      let candidateB = getValueByKey(sortKey, b);
-
-      candidateA =
-        typeof candidateA === 'number' ? candidateA.toString() : candidateA;
-      candidateB =
-        typeof candidateB === 'number' ? candidateB.toString() : candidateB;
+      const candidateA = getValueByKey(sortKey, a);
+      const candidateB = getValueByKey(sortKey, b);
 
       return (
         (candidateA === candidateB ? 0 : candidateA > candidateB ? 1 : -1) *
