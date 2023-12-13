bf815e34e82d44cafe890fb054d8f970e2e08036
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2023-12-13 16:40:48 +0530

    Add copy button to the settings page.

--- packages/extension/src/view/settings/components/tabContent/information/index.tsx
+++ packages/extension/src/view/settings/components/tabContent/information/index.tsx
@@ -17,9 +17,10 @@
 /**
  * External dependencies.
  */
-import React from 'react';
+import React, { useCallback, useEffect, useState, useRef } from 'react';
 import { useSettingsStore } from '../../../stateProviders/syncSettingsStore';
 import InformationDisplay from './informationDisplay';
+import { Copy } from '@ps-analysis-tool/design-system';
 
 const Information = () => {
   const { currentTabs, currentExtensions, browserInformation, OSInformation } =
@@ -30,23 +31,47 @@ const Information = () => {
       OSInformation: state.OSInformation,
     }));
 
-  const clipboardText = `
-  Number of open tabs: ${currentTabs}
-  Active extensions:
-  ${currentExtensions?.map((extension) => {
-    return `${extension.extensionName}: ${extension.extensionId}\n`;
-  })}
-  Browser Version: ${browserInformation}
-  OS information: ${OSInformation}
-  `;
+  const [clipboardText, setClipboardText] = useState('');
+  const [copyDone, setCopyDone] = useState(false);
+
+  const timeOutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const handleCopy = useCallback(() => {
+    //doing this instead of css because if text is too long then user might think the text has been copied and which might result in bad UX. Similar to github's copy button.
+    navigator.clipboard.writeText(clipboardText).then(() => {
+      setCopyDone(true);
+      timeOutRef.current = setTimeout(() => {
+        setCopyDone(false);
+      }, 500);
+    });
+  }, [clipboardText]);
+
+  useEffect(() => {
+    if (!copyDone && timeOutRef.current) {
+      setCopyDone(false);
+    }
+  }, [copyDone]);
+
+  useEffect(() => {
+    setClipboardText(`
+    Number of open tabs: ${currentTabs}
+    Active extensions:
+    ${currentExtensions?.map((extension) => {
+      return `${extension.extensionName}: ${extension.extensionId}\n`;
+    })}
+    Browser Version: ${browserInformation}
+    OS information: ${OSInformation}
+    `);
+  }, [OSInformation, browserInformation, currentExtensions, currentTabs]);
 
   return (
     <div className="relative w-full h-full flex flex-col gap-5 p-3 bg-dynamic-grey">
       <button
+        disabled={copyDone}
         className="absolute right-0 top-0"
-        onClick={() => navigator.clipboard.writeText(clipboardText)}
+        onClick={handleCopy}
       >
-        copy
+        <Copy className={copyDone ? 'text-mischka' : ''} />
       </button>
       <div>
         <p className="font-semibold text-lg">Current Tabs:</p>
