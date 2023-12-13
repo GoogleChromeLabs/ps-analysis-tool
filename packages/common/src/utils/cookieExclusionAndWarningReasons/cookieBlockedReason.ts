c597e13e31e4e0301b98a372a82da61898f09f69
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2023-12-13 14:32:59 +0530

    Add line spacing for blocked reason.

--- packages/common/src/utils/cookieExclusionAndWarningReasons/cookieBlockedReason.ts
+++ packages/common/src/utils/cookieExclusionAndWarningReasons/cookieBlockedReason.ts
@@ -14,35 +14,22 @@
  * limitations under the License.
  */
 const CookieBlockedReason = {
-  SecureOnly:
-    "This cookie was blocked because it had the 'Secure' attribute and the connection was not secure.",
-  NotOnPath:
-    "This cookie was blocked because its path was not an exact match for, or a superdirectory of, the request URL's path.",
-  DomainMismatch:
-    "This cookie was blocked because the request URL's domain did not exactly match the cookie's domain, nor was the request URL's domain a subdomain of the cookie's Domain attribute value.",
-  SameSiteStrict:
-    "This cookie was blocked because it had the 'SameSite=Strict' attribute and the request was made from a different site. This includes top-level navigation requests initiated by other sites.",
-  SameSiteLax:
-    "This cookie was blocked because it had the 'SameSite=Lax' attribute and the request was made from a different site and was not initiated by a top-level navigation.",
-  SameSiteUnspecifiedTreatedAsLax:
-    "This cookie didn't specify a 'SameSite' attribute when it was stored, was defaulted to 'SameSite=Lax', and was blocked because the request was made from a different site and was not initiated by a top-level navigation. The cookie had to have been set with 'SameSite=None' to enable cross-site usage.",
-  SameSiteNoneInsecure:
-    "This cookie was blocked because it had the 'SameSite=None' attribute but was not marked 'Secure'. Cookies without SameSite restrictions must be marked 'Secure' and sent over a secure connection.",
-  UserPreferences: 'This cookie was blocked due to user preferences.',
-  ThirdPartyPhaseout: 'Prepare for phasing out third-party cookies',
-  ThirdPartyBlockedInFirstPartySet:
-    'The cookie was blocked by third-party cookie blocking between sites in the same First-Party Set.',
-  UnknownError: 'Unknown error',
-  SchemefulSameSiteStrict:
-    "This cookie was blocked because it had the 'SameSite=Strict' attribute but the request was cross-site. This includes top-level navigation requests initiated by other sites. This request is considered cross-site because the URL has a different scheme than the current site.",
-  SchemefulSameSiteLax:
-    "This cookie was blocked because it had the 'SameSite=Lax' attribute but the request was cross-site and was not initiated by a top-level navigation. This request is considered cross-site because the URL has a different scheme than the current site.",
-  SchemefulSameSiteUnspecifiedTreatedAsLax:
-    "This cookie didn't specify a 'SameSite' attribute when it was stored, was defaulted to 'SameSite=Lax\"', and was blocked because the request was cross-site and was not initiated by a top-level navigation. This request is considered cross-site because the URL has a different scheme than the current site.",
-  SamePartyFromCrossPartyContext:
-    "This cookie was blocked because it had the 'SameParty' attribute but the request was cross-party. The request was considered cross-party because the domain of the resource's URL and the domains of the resource's enclosing frames/documents are neither owners nor members in the same first-party set.",
-  NameValuePairExceedsMaxSize:
-    'This cookie was blocked because it was too large. The combined size of the name and value must be less than or equal to 4,096 characters.',
+  SecureOnly: `<p>This cookie was blocked because it had the 'Secure' attribute and the connection was not secure.</p><br />`,
+  NotOnPath: `<p>This cookie was blocked because its path was not an exact match for, or a superdirectory of, the request URL's path.</p><br />`,
+  DomainMismatch: `<p>This cookie was blocked because the request URL's domain did not exactly match the cookie's domain, nor was the request URL's domain a subdomain of the cookie's Domain attribute value.</p><br />`,
+  SameSiteStrict: `<p>This cookie was blocked because it had the 'SameSite=Strict' attribute and the request was made from a different site. This includes top-level navigation requests initiated by other sites.</p><br />`,
+  SameSiteLax: `<p>This cookie was blocked because it had the 'SameSite=Lax' attribute and the request was made from a different site and was not initiated by a top-level navigation.</p><br />`,
+  SameSiteUnspecifiedTreatedAsLax: `<p>This cookie didn't specify a 'SameSite' attribute when it was stored, was defaulted to 'SameSite=Lax', and was blocked because the request was made from a different site and was not initiated by a top-level navigation. The cookie had to have been set with 'SameSite=None' to enable cross-site usage.</p><br />`,
+  SameSiteNoneInsecure: `This cookie was blocked because it had the 'SameSite=None' attribute but was not marked 'Secure'. Cookies without SameSite restrictions must be marked 'Secure' and sent over a secure connection.</p><br />`,
+  UserPreferences: `<p>This cookie was blocked due to user preferences.</p><br />`,
+  ThirdPartyPhaseout: `<p>Prepare for phasing out third-party cookies.</p><br />`,
+  ThirdPartyBlockedInFirstPartySet: `<p>The cookie was blocked by third-party cookie blocking between sites in the same First-Party Set.</p><br />`,
+  UnknownError: `<p>Unknown error.</p><br />`,
+  SchemefulSameSiteStrict: `<p>This cookie was blocked because it had the 'SameSite=Strict' attribute but the request was cross-site. This includes top-level navigation requests initiated by other sites. This request is considered cross-site because the URL has a different scheme than the current site.</p><br />`,
+  SchemefulSameSiteLax: `<p>This cookie was blocked because it had the 'SameSite=Lax' attribute but the request was cross-site and was not initiated by a top-level navigation. This request is considered cross-site because the URL has a different scheme than the current site.</p><br />`,
+  SchemefulSameSiteUnspecifiedTreatedAsLax: `<p>This cookie didn't specify a 'SameSite' attribute when it was stored, was defaulted to 'SameSite=Lax', and was blocked because the request was cross-site and was not initiated by a top-level navigation. This request is considered cross-site because the URL has a different scheme than the current site.</p><br />`,
+  SamePartyFromCrossPartyContext: `<p>This cookie was blocked because it had the 'SameParty' attribute but the request was cross-party. The request was considered cross-party because the domain of the resource's URL and the domains of the resource's enclosing frames/documents are neither owners nor members in the same first-party set.</p><br />`,
+  NameValuePairExceedsMaxSize: `<p>This cookie was blocked because it was too large. The combined size of the name and value must be less than or equal to 4,096 characters.</p><br />`,
 };
 
 export default CookieBlockedReason;
