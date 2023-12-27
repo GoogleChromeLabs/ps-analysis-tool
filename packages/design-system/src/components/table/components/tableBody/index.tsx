92229b8f5fc41be1482e6799becd431ec9164c7b
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2023-12-27 15:08:37 +0530

    Fix prettier suggestion.

--- packages/design-system/src/components/table/components/tableBody/index.tsx
+++ packages/design-system/src/components/table/components/tableBody/index.tsx
@@ -47,9 +47,9 @@ const TableBody = ({
 
   const handleKeyDown = useCallback(
     (event: React.KeyboardEvent<HTMLDivElement>, index: number) => {
-if (!['Meta', 'Control', 'c'].includes(event.key)) {
-  event.preventDefault();
-}
+      if (!['Meta', 'Control', 'c'].includes(event.key)) {
+        event.preventDefault();
+      }
       //@ts-ignore - the `children` property will be available on the `current` property.
       const currentRow = tableBodyRef.current?.children.namedItem(index);
       let newRowId: string | undefined;
