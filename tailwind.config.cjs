d662828720984652568ee4252770be164bc125ec
sayedtaqui <sayedwp@gmail.com>
2024-01-09 20:58:39 +0530

    Add new clors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -166,6 +166,10 @@ module.exports = {
       'dynamic-grey': '#fbfbfb',
       'dirty-red': '#990000',
       'dirty-pink': '#FFD6D6',
+      'jungle-green-light': '#234F1E',
+      'jungle-green-dark': '#1C4218',
+      'leaf-green-light': '#99EDC3',
+      'leaf-green-dark': '#87DFB2',
       'eerie-black': '#1F1F1F0F',
     },
     borderColor: {
