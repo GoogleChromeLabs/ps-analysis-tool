476a761ae54e2d3465ad7adabb565d0d895a3076
Mayank Rana <mayankranax1@gmail.com>
2024-02-14 13:22:19 +0530

    add timeline icon

--- packages/design-system/src/icons/index.tsx
+++ packages/design-system/src/icons/index.tsx
@@ -62,3 +62,4 @@ export { default as ClearIcon } from './clear-icon.svg';
 export { default as PaddedCross } from './padded-cross.svg';
 export { default as Warning } from './warning-filled.svg';
 export { default as VisibilityOff } from './visibility-off.svg';
+export { default as PSTimelineIcon } from './ps-timeline.svg';
