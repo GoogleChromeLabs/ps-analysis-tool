002f33191db4e5bb43d31ff51fcc8d3fae608df4
Mayank Rana <mayankranax1@gmail.com>
2024-02-15 15:29:41 +0530

    ref: import useContextSelector from common package

--- packages/cli-dashboard/src/components/siteReport/stateProviders/contentStore/index.tsx
+++ packages/cli-dashboard/src/components/siteReport/stateProviders/contentStore/index.tsx
@@ -17,12 +17,13 @@
  * External dependencies.
  */
 import React, { type PropsWithChildren, useMemo } from 'react';
-import { useContextSelector, createContext } from 'use-context-selector';
+import { createContext } from 'use-context-selector';
 import {
   type CompleteJson,
   type CookieJsonDataType,
   type CookieTableData,
   type TechnologyData,
+  useContextSelector,
 } from '@ps-analysis-tool/common';
 
 /**
