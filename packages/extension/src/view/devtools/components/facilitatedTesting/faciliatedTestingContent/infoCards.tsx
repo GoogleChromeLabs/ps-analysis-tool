fbf9afbca6f81a219e536e20cdc2806ddd97238f
sayedtaqui <sayedwp@gmail.com>
2024-02-20 12:56:25 +0530

    Update link to file issue

--- packages/extension/src/view/devtools/components/facilitatedTesting/faciliatedTestingContent/infoCards.tsx
+++ packages/extension/src/view/devtools/components/facilitatedTesting/faciliatedTestingContent/infoCards.tsx
@@ -51,9 +51,7 @@ const INFO_CARDS_DATA = [
   {
     heading: 'Reporting Breakages',
     content: `If your site or a service you depend on is breaking with third-party cookies disabled, you should file an issue <a class="text-bright-navy-blue dark:text-jordy-blue hover:opacity-80 underline" 
-    href="${addUTMParams(
-      'https://b.corp.google.com/issues/new?component=1306484&template=1777152'
-    )}" 
+    href="https://goo.gle/report-3pc-broken" 
     target="_blank">here</a>. And if you have questions or feedback about Privacy Sandbox, you can raise a new issue <a class="text-bright-navy-blue dark:text-jordy-blue hover:opacity-80 underline" 
     href="${addUTMParams(
       'https://github.com/GoogleChromeLabs/privacy-sandbox-dev-support/issues/new/choose'
