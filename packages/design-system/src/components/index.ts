cfc43dc0a563eb1a7c2ce24e5a14ff5386c12fa0
Mayank Rana <mayankranax1@gmail.com>
2024-02-26 16:40:20 +0530

    export componentS

--- packages/design-system/src/components/index.ts
+++ packages/design-system/src/components/index.ts
@@ -42,6 +42,8 @@ export * from './sidebar';
 export { default as InspectButton } from './inspectButton';
 export { default as ToastMessage } from './toastMessage';
 export {
-  default as CookiesLandingContainer,
-  type CookiesLandingContainerProps,
-} from './cookiesLanding/cookieLandingHeaderContainer';
+  default as CookiesLandingWrapper,
+  type CookiesLandingWrapperProps,
+} from './cookiesLanding/cookiesLandingWrapper';
+export { type DataMapping } from './cookiesLanding/landingHeader';
+export { default as MatrixContainer } from './matrixContainer';
