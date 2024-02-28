58b18f238df0477d2cbd1b19582409a14ac2ba37
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2024-02-28 13:51:05 +0530

    Add info icon and hover message for info icon.

--- packages/extension/src/view/devtools/app.tsx
+++ packages/extension/src/view/devtools/app.tsx
@@ -156,13 +156,15 @@ const App: React.FC = () => {
         })
         .reduce<SidebarItems>((acc, url) => {
           let popupTitle = `Cookies used by frames from ${url}`;
+          let infoIconDescription = '';
           if (url === ORPHANED_COOKIE_KEY) {
-            popupTitle =
+            popupTitle = infoIconDescription =
               'Frames that set these cookies were removed from the DOM, leaving these cookies orphaned.';
           }
 
           if (url === UNMAPPED_COOKIE_KEY) {
-            popupTitle = 'We could not map these cookies to any frame.';
+            popupTitle = infoIconDescription =
+              'We could not map these cookies to any frame.';
           }
 
           acc[url] = {
@@ -171,6 +173,7 @@ const App: React.FC = () => {
             panel: <Cookies setFilteredCookies={setFilteredCookies} />,
             icon: <CookieIcon />,
             selectedIcon: <CookieIconWhite />,
+            infoIconDescription,
             children: {},
             isBlurred: !frameHasCookies?.[url],
           };
