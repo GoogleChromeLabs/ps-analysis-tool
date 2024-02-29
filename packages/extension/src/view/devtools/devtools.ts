4ee3a417178866e7763cc9638d18ccd03c5a8f5c
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2024-02-29 20:39:25 +0530

    Fix add await to chrome.storage.session.set

--- packages/extension/src/view/devtools/devtools.ts
+++ packages/extension/src/view/devtools/devtools.ts
@@ -52,7 +52,7 @@ const callback = (panel: {
 
     const sessionData = await chrome.storage.session.get();
     sessionData[currentTabId] = false;
-    chrome.storage.session.set(sessionData);
+    await chrome.storage.session.set(sessionData);
   });
 };
 chrome.devtools.panels.create(
