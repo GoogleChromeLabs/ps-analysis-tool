23ae4987aa4b5f1f35a49281984506ba741c5dbd
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2024-03-21 13:05:06 +0530

    Move type name to constant.

--- packages/extension/src/view/devtools/stateProviders/cookie/cookieProvider.tsx
+++ packages/extension/src/view/devtools/stateProviders/cookie/cookieProvider.tsx
@@ -33,6 +33,7 @@ import {
   ALLOWED_NUMBER_OF_TABS,
   DEVTOOLS_CLOSE,
   DEVTOOLS_OPEN,
+  GET_JS_COOKIES,
   INITIAL_SYNC,
   NEW_COOKIE_DATA,
   SERVICE_WORKER_RELOAD_MESSAGE,
@@ -153,7 +154,7 @@ const Provider = ({ children }: PropsWithChildren) => {
     if (chrome.devtools.inspectedWindow.tabId && canStartInspecting) {
       await chrome.tabs.sendMessage(chrome.devtools.inspectedWindow.tabId, {
         payload: {
-          type: 'DEVTOOL::WEBPAGE::GET_JS_COOKIES',
+          type: GET_JS_COOKIES,
           tabId: chrome.devtools.inspectedWindow.tabId,
         },
       });
@@ -175,7 +176,7 @@ const Provider = ({ children }: PropsWithChildren) => {
     if (chrome.devtools.inspectedWindow.tabId) {
       await chrome.tabs.sendMessage(chrome.devtools.inspectedWindow.tabId, {
         payload: {
-          type: 'DEVTOOL::WEBPAGE::GET_JS_COOKIES',
+          type: GET_JS_COOKIES,
           tabId: chrome.devtools.inspectedWindow.tabId,
         },
       });
