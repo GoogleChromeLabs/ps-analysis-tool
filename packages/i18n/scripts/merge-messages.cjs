9af623f9e5fba0098d99d7a1508ea983481880e4
Mayank Rana <mayankranax1@gmail.com>
2024-04-05 13:23:32 +0530

    add script to merge messages json files

---
+++ packages/i18n/scripts/merge-messages.cjs
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const fs = require('fs');
+
+const packages = [
+  'cli',
+  'cli-dashboard',
+  'common',
+  'design-system',
+  'extension',
+];
+const commonPath = 'packages/i18n/_locales';
+const target = `${commonPath}/messages/en/messages.json`;
+
+const main = () => {
+  fs.writeFileSync(target, '{}');
+
+  const messages = {};
+
+  packages.forEach((pkg) => {
+    const path = `${commonPath}/packages/${pkg}/messages.json`;
+    const data = fs.readFileSync(path, 'utf8') || '{}';
+    const parsed = JSON.parse(data);
+
+    Object.entries(parsed).forEach(([key, value]) => {
+      if (messages[key]) {
+        throw new Error(
+          `Duplicate key: ${key}, found in ${pkg}. Please resolve this conflict before continuing.`
+        );
+      }
+
+      messages[key] = value;
+    });
+  });
+
+  fs.writeFileSync(target, JSON.stringify(messages, null, 2));
+};
+
+main();
