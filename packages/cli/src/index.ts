b75149e1bc274c49f247222325f7c780c004446d
nirwalayush <coding.carrots@gmail.com>
2024-04-23 12:47:55 +0530

    enhancement: make relative path relative to out dir

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -135,7 +135,15 @@ const startDashboardServer = async (dir: string, port: number) => {
       ? Utility.generatePrefix(url || sitemapUrl)
       : path.parse(csvPath || sitemapPath).name;
 
-  const outputDir = outDir ? outDir : `./out/${prefix}`;
+  let outputDir;
+
+  if (outDir && path.isAbsolute(outDir)) {
+    outputDir = outDir;
+  } else if (outDir && !path.isAbsolute(outDir)) {
+    outputDir = path.join("./out",outDir);
+  } else {
+    outputDir = `./out/${prefix}`;
+  }
 
   const spinnies = new Spinnies();
 
