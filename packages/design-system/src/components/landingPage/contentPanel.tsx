febae6a4577ed0b82d7b949ad0b46a5dbc059afb
Mayank Rana <mayankranax1@gmail.com>
2024-05-03 12:29:19 +0530

    Style changes

--- packages/design-system/src/components/landingPage/contentPanel.tsx
+++ packages/design-system/src/components/landingPage/contentPanel.tsx
@@ -22,13 +22,15 @@ import React from 'react';
 interface ContentPanelProps {
   title: string;
   content: { title: string; description: string }[];
-  extraClasses?: string;
+  titleStyles?: string;
+  counterStyles?: string;
 }
 
 const ContentPanel = ({
   title,
   content,
-  extraClasses = '',
+  titleStyles = '',
+  counterStyles = '',
 }: ContentPanelProps) => {
   return (
     <div className="px-2">
@@ -37,26 +39,25 @@ const ContentPanel = ({
       </h3>
       <div className="flex gap-5 flex-wrap">
         {content.map((item, index) => (
-          <div
+          <a
             key={index}
-            className="w-80 h-80 rounded-lg border border-bright-gray dark:border-quartz p-5"
+            href="#"
+            className="w-80 h-80 bg-[#FDFDFD] hover:bg-[#FAFAFA] rounded-xl border border-bright-gray dark:border-quartz p-5 hover:shadow hover:scale-[1.03] transition-all duration-150 ease-in-out "
           >
             <div className="w-16 h-16 flex justify-center items-center rounded-full bg-bright-gray mb-5">
               <div
-                className={`w-9 h-9 flex justify-center items-center rounded-md ${extraClasses}`}
+                className={`w-9 h-9 flex justify-center items-center rounded-md ${counterStyles}`}
               >
                 <span className="text-xxl text-white dark:black font-extrabold">
                   {index + 1}
                 </span>
               </div>
             </div>
-            <h3 className="text-xl mb-5 text-raisin-black dark:text-bright-gray">
-              {item.title}
-            </h3>
+            <h3 className={`text-xl mb-5 ${titleStyles}`}>{item.title}</h3>
             <p className="text-base text-raisin-black dark:text-bright-gray">
               {item.description}
             </p>
-          </div>
+          </a>
         ))}
       </div>
     </div>
