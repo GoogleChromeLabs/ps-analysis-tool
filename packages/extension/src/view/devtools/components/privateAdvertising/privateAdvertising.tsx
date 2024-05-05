d9c0a355ce95eadcbe06e8fce6319f15e8e284b2
Mayank Rana <58820001+mayan-000@users.noreply.github.com>
2024-05-05 10:39:33 +0530

    Update landing pages UI (#659)
    * Add component for handling content tiles and title
    
    * Increase min width for landing page
    
    * Add content to landing pages
    
    * Style changes
    
    * Add urls to content
    
    * add dark bg color
    
    * Change font size and weight
    
    * update font

--- packages/extension/src/view/devtools/components/privateAdvertising/privateAdvertising.tsx
+++ packages/extension/src/view/devtools/components/privateAdvertising/privateAdvertising.tsx
@@ -18,18 +18,48 @@
  * External Dependencies
  */
 import React from 'react';
+import { ContentPanel, LandingPage } from '@ps-analysis-tool/design-system';
 
-/**
- * Internal Dependencies
- */
-import { LandingPage } from '@ps-analysis-tool/design-system';
+const content = [
+  {
+    title: 'Protected Audience',
+    description:
+      'The Protected Audience API serves remarketing and custom audience use cases without third party cross-site tracking.',
+    url: 'https://developers.google.com/privacy-sandbox/relevance/protected-audience',
+  },
+  {
+    title: 'Attribution Reporting',
+    description:
+      'The Attribution Reporting API enables measurement of ad performance without third party cross-site tracking.',
+    url: 'https://developers.google.com/privacy-sandbox/relevance/attribution-reporting',
+  },
+  {
+    title: 'Private Aggregation',
+    description:
+      'The Private Aggregation API has been built for aggregating and reporting on cross-site data in a privacy-preserving manner.',
+    url: 'https://developers.google.com/privacy-sandbox/relevance/private-aggregation',
+  },
+  {
+    title: 'Topics',
+    description:
+      'The Topics API enables interest-based advertising while preserving user privacy.',
+    url: 'https://developers.google.com/privacy-sandbox/relevance/topics',
+  },
+];
 
 const PrivateAdvertising = () => {
   return (
     <LandingPage
       title="Private Advertising"
-      iframeSrc="https://privacysandbox.info/en/privacy-sandbox/measure-digital-ads"
-      extraClasses="h-[78vh] w-full"
+      extraClasses="min-h-[78vh] w-full"
+      contentPanel={
+        <ContentPanel
+          title="Enable your critical advertising use cases without relying on cross-site tracking via APIs providing accurate relevance information and and measurement data for digital ads."
+          content={content}
+          counterStyles="bg-blue-600"
+          titleStyles="text-blue-600"
+        />
+      }
     />
   );
 };
