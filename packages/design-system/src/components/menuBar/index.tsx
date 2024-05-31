892734ce8f95a9fcb7985a18a9d1ed3c8d37a6e3
Mayank Rana <mayankranax1@gmail.com>
2024-05-31 11:21:41 +0530

    update if conditions and logic for last element

--- packages/design-system/src/components/menuBar/index.tsx
+++ packages/design-system/src/components/menuBar/index.tsx
@@ -24,7 +24,7 @@ import { Export } from '@ps-analysis-tool/design-system';
 /**
  * Internal dependencies.
  */
-import { isInCenter } from './utils';
+import { isElementInView } from './utils';
 
 export type MenuData = Array<{
   name: string;
@@ -67,28 +67,31 @@ const MenuBar = ({
 
   useEffect(() => {
     const scrollContainer = document.getElementById(scrollContainerId);
-    const firstItemLink = menuData[0].link;
-    const lastItemLink = menuData[menuData.length - 1].link;
 
     const handleScroll = () => {
+      const firstItemLink = menuData[0].link;
+      const lastItemLink = menuData[menuData.length - 1].link;
+
       if (isListenerDisabled || !scrollContainer) {
         return;
       }
 
-      const distanceScrolled = scrollContainer.scrollTop;
-      const maxScrollDistance =
-        scrollContainer.scrollHeight - scrollContainer.clientHeight;
-
       menuData.forEach(({ link: id }) => {
         const section = document.getElementById(id);
+        const isAlmostBottom =
+          scrollContainer.scrollHeight -
+          scrollContainer.scrollTop -
+          scrollContainer.clientHeight;
+
         setSelectedItem((prev) => {
           if (scrollContainer?.scrollTop === 0) {
             return firstItemLink;
-          } else if (section && isInCenter(section) && prev !== id) {
-            return id;
-          } else if (maxScrollDistance - distanceScrolled < 5) {
+          } else if (isAlmostBottom <= 3 && isAlmostBottom >= 0) {
             return lastItemLink;
+          } else if (section && isElementInView(section)) {
+            return id;
           }
+
           return prev;
         });
       });
