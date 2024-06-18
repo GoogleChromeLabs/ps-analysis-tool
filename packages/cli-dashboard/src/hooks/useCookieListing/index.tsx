5220a9cb405ac91dd3ac03c6fa65e8709d94e3fa
nirwalayush <coding.carrots@gmail.com>
2024-06-18 11:06:14 +0530

    feat: add exemption reason filter

--- packages/cli-dashboard/src/hooks/useCookieListing/index.tsx
+++ packages/cli-dashboard/src/hooks/useCookieListing/index.tsx
@@ -27,6 +27,7 @@ import {
   calculateDynamicFilterValues,
   evaluateSelectAllOption,
   evaluateStaticFilterValues,
+  calculateExemptionReason,
 } from '@ps-analysis-tool/design-system';
 import { type CookieTableData } from '@ps-analysis-tool/common';
 
@@ -315,6 +316,26 @@ const useCookieListing = (
         comparator: (value: InfoType, filterValue: string) => {
           return (value as string[])?.includes(filterValue);
         },
+      },
+      exemptionReason: {
+        title: 'Exemption Reason',
+        hasStaticFilterValues: true,
+        hasPrecalculatedFilterValues: true,
+        enableSelectAllOption: true,
+        isSelectAllOptionSelected: evaluateSelectAllOption(
+          'exemptionReason',
+          parsedQuery
+        ),
+        filterValues: calculateExemptionReason(
+          tabCookies,
+          clearActivePanelQuery,
+          parsedQuery?.filter?.exemptionReason
+        ),
+        comparator: (value: InfoType, filterValue: string) => {
+          const val = value as string;
+          return val === filterValue;
+        },
+        useGenericPersistenceKey: true,
       },
     }),
     [clearActivePanelQuery, parsedQuery, tabCookies]
