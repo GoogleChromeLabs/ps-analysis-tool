fa31049a30d1597487a2ceb25f77e44ef6aa66cd
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2024-06-21 13:17:17 +0530

    Add try catch for tabOnUpdatedListener.

--- packages/extension/src/serviceWorker/chromeListeners/tabsOnUpdatedListener.ts
+++ packages/extension/src/serviceWorker/chromeListeners/tabsOnUpdatedListener.ts
@@ -26,59 +26,63 @@ export const onTabUpdatedListener = async (
   changeInfo: chrome.tabs.TabChangeInfo,
   tab: chrome.tabs.Tab
 ) => {
-  if (!tab.url) {
-    return;
-  }
+  try {
+    if (!tab.url) {
+      return;
+    }
 
-  const targets = await chrome.debugger.getTargets();
-  const mainFrameId = synchnorousCookieStore?.globalIsUsingCDP
-    ? targets.filter((target) => target.tabId && target.tabId === tabId)[0]?.id
-    : 0;
+    const targets = await chrome.debugger.getTargets();
+    const mainFrameId = synchnorousCookieStore?.globalIsUsingCDP
+      ? targets.filter((target) => target.tabId && target.tabId === tabId)[0]
+          ?.id
+      : 0;
 
-  const queryParams = getQueryParams(tab.url);
+    const queryParams = getQueryParams(tab.url);
 
-  if (queryParams.psat_cdp || queryParams.psat_multitab) {
-    await chrome.storage.sync.set({
-      allowedNumberOfTabs:
-        queryParams.psat_multitab === 'on' ? 'unlimited' : 'single',
-      isUsingCDP: queryParams.psat_cdp === 'on',
-    });
-
-    synchnorousCookieStore.globalIsUsingCDP = queryParams.psat_cdp === 'on';
-    synchnorousCookieStore.tabMode =
-      queryParams.psat_multitab === 'on' ? 'unlimited' : 'single';
-  }
-
-  synchnorousCookieStore?.updateUrl(tabId, tab.url);
-
-  if (
-    changeInfo.status === 'loading' &&
-    tab.url &&
-    !tab.url.startsWith('chrome://')
-  ) {
-    synchnorousCookieStore?.removeCookieData(tabId);
-
-    if (synchnorousCookieStore.globalIsUsingCDP) {
-      synchnorousCookieStore.deinitialiseVariablesForTab(tabId.toString());
-      synchnorousCookieStore.initialiseVariablesForNewTab(tabId.toString());
-
-      await attachCDP({ tabId });
-
-      const {
-        targetInfo: { targetId },
-      } = await chrome.debugger.sendCommand({ tabId }, 'Target.getTargetInfo', {
-        targetId: mainFrameId,
+    if (queryParams.psat_cdp || queryParams.psat_multitab) {
+      await chrome.storage.sync.set({
+        allowedNumberOfTabs:
+          queryParams.psat_multitab === 'on' ? 'unlimited' : 'single',
+        isUsingCDP: queryParams.psat_cdp === 'on',
       });
 
-      synchnorousCookieStore.updateParentChildFrameAssociation(
-        tabId,
-        targetId,
-        '0'
-      );
+      synchnorousCookieStore.globalIsUsingCDP = queryParams.psat_cdp === 'on';
+      synchnorousCookieStore.tabMode =
+        queryParams.psat_multitab === 'on' ? 'unlimited' : 'single';
     }
-  }
 
-  try {
+    synchnorousCookieStore?.updateUrl(tabId, tab.url);
+
+    if (
+      changeInfo.status === 'loading' &&
+      tab.url &&
+      !tab.url.startsWith('chrome://')
+    ) {
+      synchnorousCookieStore?.removeCookieData(tabId);
+
+      if (synchnorousCookieStore.globalIsUsingCDP) {
+        synchnorousCookieStore.deinitialiseVariablesForTab(tabId.toString());
+        synchnorousCookieStore.initialiseVariablesForNewTab(tabId.toString());
+
+        await attachCDP({ tabId });
+
+        const {
+          targetInfo: { targetId },
+        } = await chrome.debugger.sendCommand(
+          { tabId },
+          'Target.getTargetInfo',
+          {
+            targetId: mainFrameId,
+          }
+        );
+
+        synchnorousCookieStore.updateParentChildFrameAssociation(
+          tabId,
+          targetId,
+          '0'
+        );
+      }
+    }
     await chrome.tabs.sendMessage(tabId, {
       tabId,
       payload: {
