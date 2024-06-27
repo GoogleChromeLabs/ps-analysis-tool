30ebfad722aba2dc70a8e7f33229a656b9bbbfcf
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2024-06-27 14:39:45 +0530

    Add fallback for color and classname.

--- packages/design-system/src/utils/prepareCookieStatsComponents.ts
+++ packages/design-system/src/utils/prepareCookieStatsComponents.ts
@@ -43,7 +43,7 @@ const prepareCookieStatsComponents = (
     if (isNaN(cookieStats.blockedCookies[key])) {
       return;
     }
-
+    console.log('key', key);
     blockedCookiesStats.push({
       count: cookieStats.blockedCookies[key],
       color: COLOR_MAP[key]?.color ?? '#E59500',
@@ -52,8 +52,8 @@ const prepareCookieStatsComponents = (
     blockedCookiesLegend.push({
       label: key,
       count: cookieStats.blockedCookies[key],
-      color: COLOR_MAP[key].color ?? '#E59500',
-      countClassName: COLOR_MAP[key].className ?? 'text-jamboo',
+      color: COLOR_MAP[key]?.color ?? '#E59500',
+      countClassName: COLOR_MAP[key]?.className ?? 'text-jamboo',
     });
   });
 
@@ -67,14 +67,14 @@ const prepareCookieStatsComponents = (
 
     exemptedCookiesStats.push({
       count: cookieStats.exemptedCookies[key],
-      color: COLOR_MAP[key].color,
+      color: COLOR_MAP[key]?.color ?? '#E59500',
     });
 
     exemptedCookiesLegend.push({
       label: key,
       count: cookieStats.exemptedCookies[key],
-      color: COLOR_MAP[key].color,
-      countClassName: COLOR_MAP[key].className,
+      color: COLOR_MAP[key]?.color ?? '#E59500',
+      countClassName: COLOR_MAP[key]?.className ?? 'text-jamboo',
     });
   });
 
