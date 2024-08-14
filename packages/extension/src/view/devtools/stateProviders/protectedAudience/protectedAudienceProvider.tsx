dddcf5224e005ea1b01ba9010a30069e15c89543
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2024-08-14 10:50:21 +0530

    Descructure details in the extension.

--- packages/extension/src/view/devtools/stateProviders/protectedAudience/protectedAudienceProvider.tsx
+++ packages/extension/src/view/devtools/stateProviders/protectedAudience/protectedAudienceProvider.tsx
@@ -65,7 +65,7 @@ const Provider = ({ children }: PropsWithChildren) => {
             return {
               ...event,
               details: {
-                ...result,
+                ...result.details,
               },
             };
           })
