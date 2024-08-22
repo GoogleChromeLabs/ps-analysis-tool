22467f855f531e814b6a0b6b34a2556b8a42bc5c
sayedtaqui <sayedwp@gmail.com>
2024-08-22 10:07:28 +0530

    Install marked package and use for converting markup to html

--- packages/extension/src/view/devtools/components/wiki/index.tsx
+++ packages/extension/src/view/devtools/components/wiki/index.tsx
@@ -16,15 +16,31 @@
 /**
  * External dependencies.
  */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { marked } from 'marked';
 
 const Wiki = () => {
+  const [html, setHTML] = useState<string>('');
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        'https://raw.githubusercontent.com/wiki/GoogleChromeLabs/ps-analysis-tool/Home.md'
+      );
+      const _markdown = await response.text();
+      const _html = await marked.parse(_markdown);
+
+      setHTML(_html);
+    })();
+  }, []);
+
   return (
     <div
       data-testid="extension-settings-content"
-      className="h-full w-full flex flex-col min-w-[40rem] overflow-auto"
+      className="h-full w-full flex flex-col min-w-[40rem] overflow-auto p-5"
     >
       <h1>Wiki Page</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }}></div>
     </div>
   );
 };
