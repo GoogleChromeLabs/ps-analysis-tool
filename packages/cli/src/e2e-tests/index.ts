b2c4bfb16b94c3a4e64944e56750ea78bd735412
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2024-09-02 13:36:34 +0530

    Remove debug from CLI test.

--- packages/cli/src/e2e-tests/index.ts
+++ packages/cli/src/e2e-tests/index.ts
@@ -27,7 +27,6 @@ describe('CLI E2E Test', () => {
   it('Should run site analysis', () => {
     return coffee
       .fork(cli, ['-u https://bbc.com', '-w 100'])
-      .debug()
       .includes('stdout', '/out/bbc-com/report_')
       .end();
   }, 60000);
