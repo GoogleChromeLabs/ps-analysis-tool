af51cdaa90591e8491bb4d7ded1d74b63f24b8c8
sayedtaqui <sayedwp@gmail.com>
2024-09-02 07:05:02 +0530

    Add content and basic styling for privacy sandbox landing page

--- packages/extension/src/view/devtools/components/privacySandbox/index.tsx
+++ packages/extension/src/view/devtools/components/privacySandbox/index.tsx
@@ -17,16 +17,43 @@
  * External dependencies.
  */
 import React from 'react';
-import { LandingPage } from '@google-psat/design-system';
+import { PrivacySandboxColoredIcon } from '@google-psat/design-system';
 
 const PrivacySandbox = () => (
   <div data-testid="privacy-sandbox-content" className="h-full w-full">
-    <LandingPage
-      title="Privacy Sandbox"
-      extraClasses="h-[78vh] w-full"
-      iframeSrc="https://privacysandbox.com"
-      iframeBorderClass="border border-hex-gray dark:border-quartz"
-    />
+    <div className="p-5">
+      <header className="flex">
+        <PrivacySandboxColoredIcon width="30" height="30" className="mr-1.5" />
+        <h1 className="text-xl">Privacy Sandbox</h1>
+      </header>
+      <section className="flex justify-center pt-5">
+        <div className="max-w-screen-md text-center">
+          <PrivacySandboxColoredIcon
+            width="112"
+            height="112"
+            className="inline-block mb-5"
+          />
+          <h2 className="text-6xl mb-5 font-semibold">
+            Protecting your privacy online
+          </h2>
+          <p className="text-lg">
+            The Privacy Sandbox initiative aims to create technologies that both
+            protect people&apos;s privacy online and give companies and
+            developers tools to build thriving digital businesses. The Privacy
+            Sandbox reduces cross-site and cross-app tracking while helping to
+            keep online content and services free for all.
+          </p>
+          <div className="flex gap-1">
+            <button className="bg-cultured-grey py-2 px-4 rounded border border-dark-grey text-xl">
+              Learn About Privacy Sandbox
+            </button>
+            <button className="bg-cultured-grey py-2 px-4 rounded border border-dark-grey text-xl">
+              Dashboard
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 );
 
