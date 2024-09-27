d13ddbf94a2750c33631ba4a81266bc8e93a3c65
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2024-09-27 15:22:50 +0530

    Fix failing e2e test.

--- packages/extension/src/view/devtools/e2e-tests/allowList/index.ts
+++ packages/extension/src/view/devtools/e2e-tests/allowList/index.ts
@@ -26,7 +26,6 @@ import { Page } from 'puppeteer';
 import { PuppeteerManagement } from '../../test-utils/puppeteerManagement';
 import { Interaction } from '../../test-utils/interaction';
 
-jest.retryTimes(3);
 dotenv.config();
 
 describe('Allow Listing', () => {
@@ -68,7 +67,8 @@ describe('Allow Listing', () => {
       // Click on 'allow-list-option'
       await frame.click('#allow-list-option');
     }
-    const firstrow = await frame.$('div[title="HTMyOffer"]');
+    await interaction.delay(3000);
+    const firstrow = await frame.waitForSelector('div[title="HTMyOffer"]');
     await firstrow?.click();
 
     // Click on the green color background row from table.
