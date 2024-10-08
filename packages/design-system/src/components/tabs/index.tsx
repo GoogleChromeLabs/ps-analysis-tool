daeaa121b9681dd0c034bff0188f22f03b7ba364
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2024-10-08 11:41:33 +0530

    Fix design and remove cookies menu auto selection on page navigation.

--- packages/design-system/src/components/tabs/index.tsx
+++ packages/design-system/src/components/tabs/index.tsx
@@ -69,7 +69,7 @@ const Tabs = ({ items, activeTab, setActiveTab }: TabsProps) => {
             onClick={() => setActiveTab(index)}
             onKeyDown={handleKeyDown}
             className={classNames(
-              'pb-1.5 px-3 border-b-2 hover:opacity-80 outline-none text-sm',
+              'pb-1.5 px-1.5 border-b-2 hover:opacity-80 outline-none text-sm text-nowrap',
               {
                 'border-bright-navy-blue dark:border-jordy-blue text-bright-navy-blue dark:text-jordy-blue':
                   index === activeTab,
