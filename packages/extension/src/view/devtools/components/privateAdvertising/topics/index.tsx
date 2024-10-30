facf9bbf5dfc00f5aac857ec1708e05d82fcef8d
Mayank Rana <mayankranax1@gmail.com>
2024-10-30 10:57:39 +0530

    Add tabs to topics

--- packages/extension/src/view/devtools/components/privateAdvertising/topics/index.tsx
+++ packages/extension/src/view/devtools/components/privateAdvertising/topics/index.tsx
@@ -13,21 +13,82 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * External dependencies.
  */
-import React from 'react';
-import { LandingPage, PSInfoKey } from '@google-psat/design-system';
+import React, { useMemo, useState } from 'react';
+import {
+  InfoCard as InfoCardTemplate,
+  PSInfoKey,
+  QuickLinksList,
+  Tabs,
+  type PSInfoKeyType,
+  type TabItems,
+} from '@google-psat/design-system';
 import { I18n } from '@google-psat/i18n';
+import classNames from 'classnames';
+import TaxonomyTree from './taxonomyTree';
+
+const InfoCard = ({ infoKey }: { infoKey: PSInfoKeyType }) => {
+  return (
+    <>
+      <InfoCardTemplate infoKey={infoKey} />
+      <div className="mt-8 border-t border-gray-300 dark:border-quartz">
+        <QuickLinksList />
+      </div>
+    </>
+  );
+};
 
 const Topics = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const tabItems = useMemo<TabItems>(
+    () => [
+      {
+        title: 'Overview',
+        content: {
+          Element: InfoCard,
+          props: {
+            infoKey: PSInfoKey.Topics,
+          },
+          className: 'p-4',
+        },
+      },
+      {
+        title: 'Taxonomy Tree',
+        content: {
+          Element: TaxonomyTree,
+          props: {},
+          className: 'p-4',
+        },
+      },
+    ],
+    []
+  );
+
+  const ActiveTabContent = tabItems[activeTab].content.Element;
+
   return (
-    <div data-testid="topics-content" className="h-full w-full">
-      <LandingPage
-        title={I18n.getMessage('topics')}
-        psInfoKey={PSInfoKey.Topics}
-        extraClasses="max-w-2xl h-fit"
+    <div data-testid="topics-content" className="h-screen w-full flex flex-col">
+      <div className="p-4">
+        <div className="flex gap-2 text-2xl font-bold items-baseline text-raisin-black dark:text-bright-gray">
+          <h1 className="text-left">{I18n.getMessage('Topics')}</h1>
+        </div>
+      </div>
+      <Tabs
+        items={tabItems}
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
       />
+      <div
+        className={classNames(tabItems[activeTab].content.className, 'flex-1')}
+      >
+        {ActiveTabContent && (
+          <ActiveTabContent {...tabItems[activeTab].content.props} />
+        )}
+      </div>
     </div>
   );
 };
