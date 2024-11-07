6c93f8f02eb25cc458c67ced512fa4b61106e675
sayedtaqui <sayedwp@gmail.com>
2024-11-07 17:38:22 +0530

    Add new Explorable Explaination tab

--- packages/extension/src/view/devtools/components/webStories/index.tsx
+++ packages/extension/src/view/devtools/components/webStories/index.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Google LLC
+ * Copyright 2024 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,13 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export * from './antiCovertTracking';
-export * from './privateAdvertising';
-export * from './siteBoundaries';
-export { default as Cookies } from './cookies';
-export { default as PrivacySandbox } from './privacySandbox';
-export { default as Wiki } from './wiki';
-export { default as Settings } from './settings';
-export { default as Layout } from './layout';
-export { default as Dashboard } from './dashboard';
-export { default as WebStories } from './webStories';
+/**
+ * External dependencies.
+ */
+import React from 'react';
+
+const WebStories = () => {
+  return (
+    <div>
+      <h1>Web Stories</h1>
+    </div>
+  );
+};
+
+export default WebStories;
