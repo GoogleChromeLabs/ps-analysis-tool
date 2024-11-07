4c6a078bcb2a635ad806d75a2b6260e5d101d824
Mayank Rana <58820001+mayan-000@users.noreply.github.com>
2024-11-07 11:47:41 +0530

    Feature: Add topics taxonomy tree (#859)
    * Add tree util to create topics taxonomy svg
    
    * Add tabs to topics
    
    * Add search dropdown and event handler for opening tree from dropdown select
    
    * Color changes when selected from dropdown
    
    * Add key down handler for dropdown and input
    
    * Update search bar UI
    
    * Update clicker to check child is already open
    
    * Add V1 and V2 for taxonomony and change icon for lead nodes
    
    * Change placeholder and border
    
    * Fix tests
    
    * Add button, remove Root label and code refactoring
    
    ---------
    
    Co-authored-by: sayedtaqui <sayedwp@gmail.com>

--- packages/extension/src/view/devtools/components/privateAdvertising/topics/index.tsx
+++ packages/extension/src/view/devtools/components/privateAdvertising/topics/index.tsx
@@ -13,21 +13,102 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * External dependencies.
  */
-import React from 'react';
-import { LandingPage, PSInfoKey } from '@google-psat/design-system';
+import React, { useMemo, useState } from 'react';
+import {
+  InfoCard as InfoCardTemplate,
+  PSInfoKey,
+  QuickLinksList,
+  Tabs,
+  type PSInfoKeyType,
+  type TabItems,
+} from '@google-psat/design-system';
 import { I18n } from '@google-psat/i18n';
+import classNames from 'classnames';
+
+/**
+ * Internal dependencies.
+ */
+import TaxonomyTree from './taxonomyTree';
+
+const InfoCard = ({ infoKey }: { infoKey: PSInfoKeyType }) => {
+  return (
+    <>
+      <InfoCardTemplate infoKey={infoKey} />
+      <div className="mt-8 border-t border-gray-300 dark:border-quartz">
+        <QuickLinksList />
+      </div>
+    </>
+  );
+};
 
 const Topics = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const tabItems = useMemo<TabItems>(
+    () => [
+      {
+        title: 'Overview',
+        content: {
+          Element: InfoCard,
+          props: {
+            infoKey: PSInfoKey.Topics,
+          },
+          className: 'p-4',
+        },
+      },
+      {
+        title: 'Taxonomy V2',
+        content: {
+          Element: TaxonomyTree,
+          props: {
+            taxonomyUrl:
+              'https://raw.githubusercontent.com/patcg-individual-drafts/topics/refs/heads/main/taxonomy_v2.md',
+            githubUrl:
+              'https://github.com/patcg-individual-drafts/topics/blob/main/taxonomy_v2.md',
+          },
+        },
+      },
+      {
+        title: 'Taxonomy V1',
+        content: {
+          Element: TaxonomyTree,
+          props: {
+            taxonomyUrl:
+              'https://raw.githubusercontent.com/patcg-individual-drafts/topics/refs/heads/main/taxonomy_v1.md',
+            githubUrl:
+              'https://github.com/patcg-individual-drafts/topics/blob/main/taxonomy_v1.md',
+          },
+        },
+      },
+    ],
+    []
+  );
+
+  const ActiveTabContent = tabItems[activeTab].content.Element;
+
   return (
-    <div data-testid="topics-content" className="h-full w-full">
-      <LandingPage
-        title={I18n.getMessage('topics')}
-        psInfoKey={PSInfoKey.Topics}
-        extraClasses="max-w-2xl h-fit"
+    <div data-testid="topics-content" className="h-screen w-full flex flex-col">
+      <div className="p-4">
+        <div className="flex gap-2 text-2xl font-bold items-baseline text-raisin-black dark:text-bright-gray">
+          <h1 className="text-left">{I18n.getMessage('Topics')}</h1>
+        </div>
+      </div>
+      <Tabs
+        items={tabItems}
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
       />
+      <div
+        className={classNames(tabItems[activeTab].content.className, 'flex-1')}
+      >
+        {ActiveTabContent && (
+          <ActiveTabContent {...tabItems[activeTab].content.props} />
+        )}
+      </div>
     </div>
   );
 };
