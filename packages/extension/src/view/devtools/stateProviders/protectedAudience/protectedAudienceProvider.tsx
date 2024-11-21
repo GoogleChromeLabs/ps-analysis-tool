dafdcda21c448bfadec9963c401f6aa77ce1c395
Mayank Rana <mayankranax1@gmail.com>
2024-11-21 12:46:45 +0530

    Change early return condition

--- packages/extension/src/view/devtools/stateProviders/protectedAudience/protectedAudienceProvider.tsx
+++ packages/extension/src/view/devtools/stateProviders/protectedAudience/protectedAudienceProvider.tsx
@@ -341,8 +341,7 @@ const Provider = ({ children }: PropsWithChildren) => {
       tabId,
     }: chrome.webNavigation.WebNavigationFramedCallbackDetails) => {
       if (
-        frameType !== 'outermost_frame' &&
-        frameId !== 0 &&
+        (frameType !== 'outermost_frame' && frameId !== 0) ||
         tabId !== chrome.devtools.inspectedWindow.tabId
       ) {
         return;
