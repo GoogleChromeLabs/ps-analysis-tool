ffbd4206eeebe3ee6bc2b74bdcc87eff58f904c8
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2024-11-25 23:23:16 +0530

    Add titles.

--- packages/extension/src/view/devtools/components/privateAdvertising/protectedAudience/explorableExplanation/index.tsx
+++ packages/extension/src/view/devtools/components/privateAdvertising/protectedAudience/explorableExplanation/index.tsx
@@ -135,12 +135,17 @@ const ExplorableExplanation = () => {
         <div className="flex gap-0.5">
           <button
             id="prevButton"
+            title="Previous Node"
             onClick={app.handlePrevButton}
             className="disabled:opacity-50 disabled:pointer-events-none"
           >
             <PreviousIcon className="h-5 w-5 hover:opacity-70 active:opacity-50" />
           </button>
-          <button onClick={app.handleNextButton} id="nextButton">
+          <button
+            onClick={app.handleNextButton}
+            id="nextButton"
+            title="Next Node"
+          >
             <NextIcon className="h-5 w-5 hover:opacity-70 active:opacity-50" />
           </button>
         </div>
