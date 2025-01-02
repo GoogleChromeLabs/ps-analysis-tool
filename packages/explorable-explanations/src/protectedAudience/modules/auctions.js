a864eb570ba39e3319c8f92fac2ab7975a47138a
sayedtaqui <sayedwp@gmail.com>
2025-01-02 18:37:11 +0530

    Create Publisher Ad Server Flow

--- packages/explorable-explanations/src/protectedAudience/modules/auctions.js
+++ packages/explorable-explanations/src/protectedAudience/modules/auctions.js
@@ -67,8 +67,8 @@ auction.setUp = (index) => {
     auction.setUpSingleSellerFirstSSPTagFlow();
   }
 
-  auction.setUpRunadAuction();
-  auction.setupShowWinningAd();
+  // auction.setUpRunadAuction();
+  // auction.setupShowWinningAd();
 
   app.auction.auctions.push(auction.steps);
 };
@@ -403,6 +403,69 @@ auction.setUpPublisherAdServerFlow = () => {
       y: () => app.auction.nextTipCoordinates?.y - box.height / 2,
     },
     delay: 1000,
+    callBack: (returnValue) => {
+      app.auction.nextTipCoordinates = returnValue.down;
+    },
+  });
+
+  auction.steps.push({
+    component: ProgressLine,
+    props: {
+      direction: 'right',
+      x1: () => app.auction.nextTipCoordinates?.x + box.width / 2,
+      y1: () => app.auction.nextTipCoordinates?.y + 10,
+    },
+    callBack: (returnValue) => {
+      app.auction.nextTipCoordinates = returnValue;
+    },
+  });
+
+  auction.steps.push({
+    component: Box,
+    props: {
+      title: 'Publisher',
+      description: 'Ad server',
+      x: () => app.auction.nextTipCoordinates?.x + 10,
+      y: () => app.auction.nextTipCoordinates?.y - 23,
+    },
+    delay: 1000,
+    callBack: (returnValue) => {
+      app.auction.nextTipCoordinates = returnValue.down;
+    },
+  });
+
+  auction.steps.push({
+    component: ProgressLine,
+    props: {
+      direction: 'left',
+      x1: () => app.auction.nextTipCoordinates?.x + box.width / 4 + 6,
+      y1: () => app.auction.nextTipCoordinates?.y + 10,
+    },
+    callBack: (returnValue) => {
+      app.auction.nextTipCoordinates = returnValue;
+    },
+  });
+
+  auction.steps.push({
+    component: ProgressLine,
+    props: {
+      direction: 'down',
+      x1: () => app.auction.nextTipCoordinates?.x - box.width / 2 - 10,
+      y1: () => app.auction.nextTipCoordinates?.y + box.height / 2 - 5,
+    },
+    callBack: (returnValue) => {
+      app.auction.nextTipCoordinates = returnValue;
+    },
+  });
+
+  auction.steps.push({
+    component: Box,
+    props: {
+      title: 'runAdAuction',
+      x: () => app.auction.nextTipCoordinates?.x - box.width / 2,
+      y: () => app.auction.nextTipCoordinates?.y + box.height / 4,
+    },
+    delay: 1111000,
     callBack: (returnValue) => {
       app.auction.nextTipCoordinates = returnValue.down;
     },
