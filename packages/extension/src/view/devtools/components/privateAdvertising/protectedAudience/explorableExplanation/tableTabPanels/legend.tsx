41ac07308d74ec210b528288070075f00af4b796
Mohammad Taqui Sayed <6297436+mohdsayed@users.noreply.github.com>
2025-02-14 11:45:09 +0530

    Explorable Explanation: Add legend content and remove 'General Use' from info tab (#951)
    * Remove 'General Use' from info text
    
    * Add interest groups circles in legend
    
    * Add content to legend tab
    
    * Fix typo and color in dark mode
    
    * Introduce a new Link component that opens links within it's own tab

--- packages/extension/src/view/devtools/components/privateAdvertising/protectedAudience/explorableExplanation/tableTabPanels/legend.tsx
+++ packages/extension/src/view/devtools/components/privateAdvertising/protectedAudience/explorableExplanation/tableTabPanels/legend.tsx
@@ -18,17 +18,49 @@
  * External dependencies.
  */
 import React from 'react';
+import { Link } from '@google-psat/design-system';
 
 const Legend = () => {
   return (
-    <div className="p-4 flex flex-col gap-3">
-      <div className="flex gap-2 items-center">
-        <div className="bg-yellow-400 w-4 h-4 border border-black" />
-        <p>Yellow boxes signify processes running out of browser context.</p>
-      </div>
-      <div className="flex gap-2 items-center">
-        <div className="bg-white w-4 h-4 border border-black" />
-        <p>White boxes signify processes running inside browser context.</p>
+    <div className="p-3 text-[12.5px] dark:text-bright-gray">
+      <p>
+        This{' '}
+        <Link href="https://en.wikipedia.org/wiki/Explorable_explanation">
+          explorable explanation
+        </Link>{' '}
+        illustrates the generic flow outlined in the{' '}
+        <Link href="https://developers.google.com/display-video/protected-audience/ssp-guide">
+          ssp-guide
+        </Link>{' '}
+        diagrams using synthetic data for both single and multi-seller auctions.
+        The timeline follows a user’s journey from visiting advertiser websites,
+        where interest groups are added, to publisher websites, where auctions
+        take place. Click the info icon on each box to learn more about the
+        process. Below is a guide to the different symbols used in the diagram.
+      </p>
+      <div className="flex flex-col gap-3 mt-3">
+        <div className="flex gap-2 items-center">
+          <div className="bg-yellow-400 w-4 h-4 border border-black" />
+          <p>Yellow boxes signify processes running out of browser context.</p>
+        </div>
+        <div className="flex gap-2 items-center">
+          <div className="bg-white w-4 h-4 border border-black" />
+          <p>
+            White boxes signify processes running inside the browser context.
+          </p>
+        </div>
+        <div className="flex gap-2 items-center">
+          <div className="w-4 h-4">
+            <div className="flex justify-center w-4">
+              <div className="rounded-full w-2 h-2 bg-[#4e79a7]"></div>
+            </div>
+            <div className="flex">
+              <div className="rounded-full w-2 h-2 bg-[#e15759]"></div>
+              <div className="rounded-full w-2 h-2 bg-[#f28e2c]"></div>
+            </div>
+          </div>
+          <p>Small bubbles represent interest groups.</p>
+        </div>
       </div>
     </div>
   );
