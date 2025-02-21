39ba0d3555f7b2723487ee382fea1077ea79db68
sayedtaqui <sayedwp@gmail.com>
2025-02-21 08:31:23 +0530

    Increase font size of evavluation env notice

--- packages/extension/src/view/devtools/components/privateAdvertising/protectedAudience/bids/receivedBidsTable.tsx
+++ packages/extension/src/view/devtools/components/privateAdvertising/protectedAudience/bids/receivedBidsTable.tsx
@@ -213,7 +213,7 @@ const ReceivedBidsTable = ({
   if (!receivedBids || receivedBids.length === 0) {
     return (
       <div className="w-full h-full flex flex-col items-center justify-center">
-        <p className="text-sm text-raisin-black dark:text-bright-gray">
+        <p className="text-lg text-raisin-black dark:text-bright-gray">
           No bids data was recorded.
         </p>
         {showEvaluationPlaceholder && (
