690fd3cdbedc12d703ad9458a42fbf33e44b7e7b
Carlos Barrantes <carlosbarrantesb@gmail.com>
2025-02-26 22:20:14 +0100

    add mock to unit test

--- packages/extension/src/view/devtools/components/antiCovertTracking/tests/index.tsx
+++ packages/extension/src/view/devtools/components/antiCovertTracking/tests/index.tsx
@@ -63,6 +63,8 @@ describe('AntiCovertTracking Landing Pages', () => {
       },
     });
 
+    globalThis.requestIdleCallback = jest.fn();
+
     mockUseSidebar.mockReturnValue({ extractSelectedItemKeyTitles: () => [] });
   });
 
