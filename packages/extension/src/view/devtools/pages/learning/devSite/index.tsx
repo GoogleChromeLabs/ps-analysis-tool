4281fea96d4163f79028fdc68f261314bcbb832f
Carlos Barrantes <carlosbarrantesb@gmail.com>
2025-02-28 10:52:30 +0100

    improve description logic

--- packages/extension/src/view/devtools/pages/learning/devSite/index.tsx
+++ packages/extension/src/view/devtools/pages/learning/devSite/index.tsx
@@ -20,19 +20,22 @@
 import { DevGuideIcon, SIDEBAR_ITEMS_KEYS } from '@google-psat/design-system';
 import React from 'react';
 
+/**
+ * Internal dependencies.
+ */
 import { ITEMS } from '../contentPanel';
 
-const description = ITEMS.find(
-  (item) => item.sidebarKey === SIDEBAR_ITEMS_KEYS.DEV_SITE
-)?.description;
-
 const DevSite = () => {
+  const description =
+    ITEMS.find((item) => item.sidebarKey === SIDEBAR_ITEMS_KEYS.DEV_SITE)
+      ?.description ?? '';
+
   return (
     <div className="w-full h-full overflow-hidden">
       <div className="w-full h-full flex justify-center items-center">
         <p className="text-center text-lg flex flex-col items-center gap-2 text-raisin-black dark:text-bright-gray">
           <DevGuideIcon className="w-10 h-10 fill-granite-gray dark:fill-bright-gray" />
-          <p>{description}</p>
+          {description && <p>{description}</p>}
           <p className="text-sm">
             Please refer to the developer site opened in the current browser
             tab.
