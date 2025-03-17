152be37fd4abe59c707e6c7173da09588c76fdc2
Carlos Barrantes <carlosbarrantesb@gmail.com>
2025-03-17 13:15:45 +0100

    rename files in git

--- packages/explorable-explanations/src/topics/components/Circle.ts
+++ packages/explorable-explanations/src/topics/components/Circle.ts
@@ -13,7 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * External dependencies
+ */
 import p5 from 'p5';
+
+/**
+ * Internal dependencies
+ */
 import { config } from '../config';
 
 type CircleProps = {
