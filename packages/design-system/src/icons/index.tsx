5bac729a327de9dc8f2a9cb5b9a4822bac320d72
Mohammad Taqui Sayed <6297436+mohdsayed@users.noreply.github.com>
2025-04-03 19:19:56 +0530

    Add search icon to info cards (#1030)
    * Add search icon to the card
    
    * Add search url
    
    * Remove search url from private advertising

--- packages/design-system/src/icons/index.tsx
+++ packages/design-system/src/icons/index.tsx
@@ -111,4 +111,5 @@ export { default as WikiLandingIcon } from './wiki-landing.svg';
 export { default as DemosLandingIcon } from './demos-landing.svg';
 export { default as DevSiteLandingIcon } from './dev-site-landing.svg';
 export { default as HelpCenterLandingIcon } from './help-center-landing.svg';
+export { default as SearchIcon } from './search.svg';
 export * from './ps-icons';
