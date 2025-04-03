fdcd878dfdc55ae99f5429c6c03e6cb78dba7093
Carlos Barrantes <carlosbarrantesb@gmail.com>
2025-04-03 18:06:26 +0200

    fix storybook set up and remove vulnerabilites

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,21 +6,36 @@ import type { RuleSetRule } from 'webpack';
 
 const config: StorybookConfig = {
   stories: [
-    '../packages/extension/src/**/stories/*.mdx',
-    '../packages/extension/src/**/stories/**/*.stories.@(js|jsx|ts|tsx)',
     '../packages/design-system/src/**/stories/*.mdx',
-    '../packages/design-system/src/**/stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../packages/design-system/src/**/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../packages/extension/src/**/stories/*.mdx',
+    '../packages/extension/src/**/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
-    '@storybook/addon-links',
+    '@storybook/addon-webpack5-compiler-swc',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     {
-      name: '@storybook/addon-styling',
+      name: '@storybook/addon-styling-webpack',
       options: {
-        postCss: {
-          implementation: require('postcss'),
-        },
+        rules: [
+          // Replaces existing CSS rules to support PostCSS
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: { importLoaders: 1 },
+              },
+              {
+                // Gets options from `postcss.config.js` in your project root
+                loader: 'postcss-loader',
+                options: { implementation: require.resolve('postcss') },
+              },
+            ],
+          },
+        ],
       },
     },
   ],
@@ -33,11 +48,13 @@ const config: StorybookConfig = {
   },
   webpackFinal: (config) => {
     // Default rule for images /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
-    //@todo find the right method to correct this error.
-    // @ts-ignore this property exists on webpack RuleSetRule still ts is showing error.
-    const fileLoaderRule = config?.module?.rules?.find(
-      (rule) => rule.test && rule.test.test('.svg')
-    );
+    const fileLoaderRule = config?.module?.rules?.find((rule) => {
+      const test = (rule as RuleSetRule)?.test;
+      if (typeof test === 'string' && test.includes('.svg')) {
+        return true;
+      }
+      return false;
+    });
     if (fileLoaderRule) {
       // @ts-ignore this property exists on webpack RuleSetRule still ts is showing error.
       fileLoaderRule.exclude = /\.svg$/;
@@ -53,10 +70,13 @@ const config: StorybookConfig = {
       // Fixes resolving packages in the monorepo so we use the "src" folder, not "dist".
       exportsFields: ['customExports', 'exports'],
       extensions: ['.ts', '.tsx', '.js'],
+      fallback: {
+        path: false,
+        fs: false,
+      },
     };
 
     return config;
   },
 };
-
 export default config;
