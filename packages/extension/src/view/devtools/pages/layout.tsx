4f376cf5f4af73c946ff476d3b087f561831bb0a
Mayank Rana <58820001+mayan-000@users.noreply.github.com>
2025-04-04 23:06:19 +0530

    Fix: Redirection to dashboard (#1029)
    * Remove dashboard as default value if no selected frame
    
    * Remove fallback useeffect to highlight cookies sidear if selected frame is null
    
    * If key is not found try ancestors
    
    * Remove console log
    
    * Fix E2E test failure.
    
    * Fix failing E2E test
    
    * Make headless true
    
    * Fix failing E2E test.
    
    ---------
    
    Co-authored-by: sayedtaqui <sayedwp@gmail.com>
    Co-authored-by: Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>

--- packages/extension/src/view/devtools/pages/layout.tsx
+++ packages/extension/src/view/devtools/pages/layout.tsx
@@ -89,10 +89,8 @@ const Layout = ({ setSidebarData }: LayoutProps) => {
     tabFrames,
     frameHasCookies,
     canStartInspecting,
-    tabUrl,
     isInspecting,
     setIsInspecting,
-    selectedFrame,
     setSelectedFrame,
   } = useCookie(({ state, actions }) => ({
     tabFrames: state.tabFrames,
@@ -250,22 +248,6 @@ const Layout = ({ setSidebarData }: LayoutProps) => {
       );
     })();
   }, [selectedItemKey, isCollapsed, cookieDropdownOpen]);
-
-  const lastUrl = useRef(tabUrl);
-
-  useEffect(() => {
-    if (
-      lastUrl.current === null ||
-      new URL(lastUrl.current).hostname === new URL(tabUrl || '').hostname
-    ) {
-      lastUrl.current = tabUrl;
-      return;
-    }
-
-    lastUrl.current = tabUrl;
-
-    updateSelectedItemKey(selectedFrame || SIDEBAR_ITEMS_KEYS.DASHBOARD);
-  }, [selectedFrame, tabUrl, updateSelectedItemKey]);
 
   const [filteredCookies, setFilteredCookies] = useState<CookieTableData[]>([]);
 
