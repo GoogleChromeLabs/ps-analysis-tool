705411fc7c5f8ed937fc2c17697b11e88c252726
Mayank Rana <mayankranax1@gmail.com>
2025-04-18 17:01:38 +0530

    Add texts describing arrow role to multiseller

--- packages/explorable-explanations/src/protectedAudience/modules/auctions/multi-seller/setUpFirstSSPTagFlow.ts
+++ packages/explorable-explanations/src/protectedAudience/modules/auctions/multi-seller/setUpFirstSSPTagFlow.ts
@@ -63,6 +63,7 @@ const setUpMultiSellerFirstSSPTagFlow = (steps: AuctionStep[]) => {
       direction: 'down',
       x1: () => getCoordinateValues(app.auction.nextTipCoordinates).x,
       y1: () => getCoordinateValues(app.auction.nextTipCoordinates).y + 40,
+      text: 'Ad request',
     },
     callBack: (returnValue) => {
       app.auction.nextTipCoordinates = returnValue;
@@ -95,6 +96,7 @@ const setUpMultiSellerFirstSSPTagFlow = (steps: AuctionStep[]) => {
       direction: 'down',
       x1: () => getCoordinateValues(app.auction.nextTipCoordinates).x,
       y1: () => getCoordinateValues(app.auction.nextTipCoordinates).y + 40,
+      text: 'OpenRTB \n bid request',
     },
     callBack: (returnValue) => {
       app.auction.nextTipCoordinates = returnValue;
@@ -127,6 +129,7 @@ const setUpMultiSellerFirstSSPTagFlow = (steps: AuctionStep[]) => {
       direction: 'up',
       x1: () => getCoordinateValues(app.auction.nextTipCoordinates).x + 10,
       y1: () => getCoordinateValues(app.auction.nextTipCoordinates).y - 15,
+      text: 'OpenRTB \n bid response',
     },
     callBack: (returnValue) => {
       app.auction.nextTipCoordinates = returnValue;
