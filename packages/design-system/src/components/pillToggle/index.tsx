ac2a0df4654a731bc9610d25ad5c3b9f3867841c
Mayank Rana <mayankranax1@gmail.com>
2025-05-19 15:30:48 +0530

    Add pill to adunits, make admatrix dummy

--- packages/design-system/src/components/pillToggle/index.tsx
+++ packages/design-system/src/components/pillToggle/index.tsx
@@ -24,7 +24,7 @@ interface PillToggleProps {
   secondOption: string;
   pillToggle: string;
   setPillToggle: (value: string) => void;
-  eeAnimatedTab: boolean;
+  eeAnimatedTab?: boolean;
 }
 
 const PillToggle = ({
