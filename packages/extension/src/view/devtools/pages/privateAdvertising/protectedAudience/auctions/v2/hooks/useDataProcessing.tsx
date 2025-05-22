c6307b9bfe99e06d8ba67ccbb6e0d985d34f70cd
Mayank Rana <mayankranax1@gmail.com>
2025-05-22 16:45:20 +0530

    Add hooks for data processing and sidebar data creation

---
+++ packages/extension/src/view/devtools/pages/privateAdvertising/protectedAudience/auctions/v2/hooks/useDataProcessing.tsx
@@ -0,0 +1,345 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies.
+ */
+import { useCallback, useMemo } from 'react';
+import { useProtectedAudience } from '../../../../../../stateProviders';
+
+const useDataProcessing = () => {
+  const { paapi, prebidResponse, isMultiSeller } = useProtectedAudience(
+    ({ state }) => ({
+      paapi: {
+        auctionEvents: state.auctionEvents,
+        adsAndBidders: state.adsAndBidders,
+        isMultiSeller: state.isMultiSellerAuction,
+        receivedBids: state.receivedBids,
+        noBids: state.noBids,
+      },
+      prebidResponse: state.prebidResponse,
+      isMultiSeller: state.isMultiSellerAuction,
+    })
+  );
+
+  const adUnits = useMemo(() => {
+    if (Object.keys(prebidResponse?.adUnits || {}).length > 0) {
+      return Object.keys(prebidResponse.adUnits);
+    }
+
+    return Object.keys(paapi.adsAndBidders);
+  }, [paapi.adsAndBidders, prebidResponse.adUnits]);
+
+  const adUnitsTimestamp = useMemo(() => {
+    const _adUnitsTimestamp: Record<string, string[]> = {};
+
+    if (Object.keys(prebidResponse?.adUnits || {}).length > 0) {
+      Object.values(prebidResponse?.auctionEvents || {}).forEach((events) => {
+        const _adUnits = events[0].adUnitCodes as string[];
+
+        _adUnits.forEach((adUnit) => {
+          _adUnitsTimestamp[adUnit] = [
+            ...(_adUnitsTimestamp[adUnit] || []),
+            new Date(events[0].timestamp).toISOString(),
+          ];
+        });
+      });
+    }
+
+    if (Object.keys(paapi.adsAndBidders).length > 0) {
+      const _adUnits = Object.keys(paapi.adsAndBidders);
+
+      _adUnits.forEach((adUnit) => {
+        const events = paapi.auctionEvents?.[adUnit];
+
+        if (Object.keys(events || {}).length > 0) {
+          Object.keys(events || {}).forEach((time) => {
+            _adUnitsTimestamp[adUnit] = [
+              ...(_adUnitsTimestamp[adUnit] || []),
+              time.split('||')[0],
+            ];
+          });
+        }
+      });
+    }
+
+    return _adUnitsTimestamp;
+  }, [
+    paapi.adsAndBidders,
+    paapi.auctionEvents,
+    prebidResponse.adUnits,
+    prebidResponse.auctionEvents,
+  ]);
+
+  const adUnitsAuctionId = useMemo(() => {
+    const _adUnitsAuctionId: Record<string, Record<string, string>> = {};
+
+    if (Object.keys(prebidResponse.adUnits).length > 0) {
+      Object.values(prebidResponse.auctionEvents).forEach((events) => {
+        const _adUnits = events[0].adUnitCodes as string[];
+        const time = new Date(events[0].timestamp).toISOString();
+
+        _adUnits.forEach((adUnit) => {
+          if (!_adUnitsAuctionId[adUnit]) {
+            _adUnitsAuctionId[adUnit] = {};
+          }
+
+          _adUnitsAuctionId[adUnit][time] = events[0].auctionId;
+        });
+      });
+    } else if (Object.keys(paapi.adsAndBidders).length > 0) {
+      const _adUnits = Object.keys(paapi.adsAndBidders);
+
+      _adUnits.forEach((adUnit) => {
+        const events = paapi.auctionEvents?.[adUnit];
+
+        if (Object.keys(events || {}).length > 0) {
+          Object.keys(events || {}).forEach((time) => {
+            if (!_adUnitsAuctionId[adUnit]) {
+              _adUnitsAuctionId[adUnit] = {};
+            }
+
+            _adUnitsAuctionId[adUnit][time.split('||')[0]] =
+              time.split('||')[1];
+          });
+        }
+      });
+    }
+
+    return _adUnitsAuctionId;
+  }, [
+    paapi.adsAndBidders,
+    paapi.auctionEvents,
+    prebidResponse.adUnits,
+    prebidResponse.auctionEvents,
+  ]);
+
+  const adUnitsMediaContainerSize = useMemo(() => {
+    const _adUnitsMediaContainerSizeStore: Record<string, Set<string>> = {};
+
+    if (Object.keys(prebidResponse.adUnits).length > 0) {
+      Object.values(prebidResponse.adUnits).forEach((data) => {
+        const mediaContainerSize = data.mediaContainerSize as number[][];
+
+        if (mediaContainerSize) {
+          if (!_adUnitsMediaContainerSizeStore[data.adUnitCode]) {
+            _adUnitsMediaContainerSizeStore[data.adUnitCode] = new Set();
+          }
+
+          _adUnitsMediaContainerSizeStore[data.adUnitCode].add(
+            mediaContainerSize.join('x')
+          );
+        }
+      });
+    }
+
+    if (Object.keys(paapi.adsAndBidders).length > 0) {
+      Object.values(paapi.adsAndBidders).forEach((data) => {
+        const mediaContainerSize = data.mediaContainerSize as number[][];
+
+        if (mediaContainerSize) {
+          if (!_adUnitsMediaContainerSizeStore[data.adUnitCode]) {
+            _adUnitsMediaContainerSizeStore[data.adUnitCode] = new Set();
+          }
+
+          _adUnitsMediaContainerSizeStore[data.adUnitCode].add(
+            mediaContainerSize.join('x')
+          );
+        }
+      });
+    }
+
+    const _adUnitsMediaContainerSize = Object.entries(
+      _adUnitsMediaContainerSizeStore
+    ).reduce((acc, [adunit, sizes]) => {
+      const parsedSizes = Array.from(sizes).map((size) => {
+        const [width, height] = size.split('x').map(Number);
+        return [width, height];
+      });
+
+      if (!acc[adunit]) {
+        acc[adunit] = [];
+      }
+
+      acc[adunit] = parsedSizes;
+
+      return acc;
+    }, {} as Record<string, number[][]>);
+
+    return _adUnitsMediaContainerSize;
+  }, [paapi.adsAndBidders, prebidResponse.adUnits]);
+
+  const adUnitsBidders = useMemo(() => {
+    const _adUnitBidders: Record<string, string[]> = {};
+    if (Object.keys(prebidResponse.adUnits).length > 0) {
+      Object.values(prebidResponse.adUnits).forEach((data) => {
+        const bidders = data.bidders as string[];
+
+        if (bidders) {
+          _adUnitBidders[data.adUnitCode] = bidders;
+        }
+      });
+    }
+
+    if (Object.keys(paapi.adsAndBidders).length > 0) {
+      Object.values(paapi.adsAndBidders).forEach((data) => {
+        const bidders = data.bidders as string[];
+
+        if (bidders) {
+          _adUnitBidders[data.adUnitCode] = bidders;
+        }
+      });
+    }
+
+    return _adUnitBidders;
+  }, [paapi.adsAndBidders, prebidResponse.adUnits]);
+
+  const adUnitsBidsCount = useMemo(() => {
+    const _adUnitBidsCount: Record<string, number> = {};
+
+    if (Object.keys(prebidResponse.receivedBids).length > 0) {
+      Object.values(prebidResponse.receivedBids).forEach((data) => {
+        const adUnit = data.adUnitCode as string;
+
+        if (adUnit) {
+          _adUnitBidsCount[adUnit] = (_adUnitBidsCount[adUnit] || 0) + 1;
+        }
+      });
+    }
+
+    if (Object.keys(paapi.receivedBids).length > 0) {
+      Object.values(paapi.receivedBids).forEach((data) => {
+        const adUnit = data.adUnitCode as string;
+
+        if (adUnit) {
+          _adUnitBidsCount[adUnit] = (_adUnitBidsCount[adUnit] || 0) + 1;
+        }
+      });
+    }
+
+    return _adUnitBidsCount;
+  }, [paapi.receivedBids, prebidResponse.receivedBids]);
+
+  const adUnitsNoBidsCount = useMemo(() => {
+    const _adUnitNoBidsCount: Record<string, number> = {};
+
+    if (Object.keys(prebidResponse.noBids).length > 0) {
+      Object.values(prebidResponse.noBids).forEach((data) => {
+        const adUnit = data.adUnitCode as string;
+
+        if (adUnit) {
+          _adUnitNoBidsCount[adUnit] = (_adUnitNoBidsCount[adUnit] || 0) + 1;
+        }
+      });
+    }
+
+    if (Object.keys(paapi.noBids).length > 0) {
+      Object.values(paapi.noBids).forEach((data) => {
+        const adUnit = data.adUnitCode as string;
+
+        if (adUnit) {
+          _adUnitNoBidsCount[adUnit] = (_adUnitNoBidsCount[adUnit] || 0) + 1;
+        }
+      });
+    }
+
+    return _adUnitNoBidsCount;
+  }, [paapi.noBids, prebidResponse.noBids]);
+
+  const getPrebidData = useCallback(
+    (adUnit: string, time: string) => {
+      const auction = Object.entries(prebidResponse.auctionEvents).find(
+        ([, events]) => {
+          return (
+            events[0].adUnitCodes.includes(adUnit) &&
+            new Date(events[0].timestamp).toISOString() === time
+          );
+        }
+      );
+
+      return auction;
+    },
+    [prebidResponse.auctionEvents]
+  );
+
+  const getPAData = useCallback(
+    (adUnit: string, time: string, parentAuctionId: string) => {
+      const adUnitEvents =
+        paapi.auctionEvents?.[adUnit]?.[time + '||' + parentAuctionId] || {};
+
+      const sellerUrl = Object.keys(adUnitEvents)[0];
+
+      const nonSplittedSellerUrl =
+        Object.keys(adUnitEvents?.[sellerUrl] || {})?.[0] || '';
+
+      const entries = Object.entries(adUnitEvents[sellerUrl] || {})
+        .filter(([url]) => {
+          const splittedUrl = url.split('||');
+
+          if (isMultiSeller) {
+            return !(
+              splittedUrl[0] === sellerUrl && splittedUrl[1] === parentAuctionId
+            );
+          }
+          return splittedUrl[0] !== sellerUrl;
+        })
+        .reduce((acc, [url, events]) => {
+          acc[url.split('||')[0] + time + adUnit] = {
+            key: url.split('||')[0] + time + adUnit,
+            title: events[0]?.auctionConfig?.seller ?? url.split('||')[0],
+            auctionEvents: events,
+            parentOrigin: events[0]?.auctionConfig?.seller,
+            startDate: time,
+            isBlurred: events.length === 0,
+          };
+
+          return acc;
+        }, {} as Record<string, unknown>);
+
+      const adUnitdata = {
+        [`${time}||${parentAuctionId}||${adUnit}`]: {
+          key: `${time}||${parentAuctionId}||${adUnit}`,
+          title: 'PAAPI',
+          auctionEvents:
+            adUnitEvents?.[sellerUrl]?.[nonSplittedSellerUrl] || [],
+          parentOrigin:
+            adUnitEvents?.[sellerUrl]?.[nonSplittedSellerUrl][0]?.auctionConfig
+              ?.seller || '',
+          startDate: time,
+          children: entries,
+          isBlurred: !adUnitEvents[sellerUrl]?.[sellerUrl]?.length,
+        },
+      };
+
+      return adUnitdata;
+    },
+    [isMultiSeller, paapi.auctionEvents]
+  );
+
+  return {
+    adUnits,
+    adUnitsTimestamp,
+    adUnitsAuctionId,
+    adUnitsMediaContainerSize,
+    adUnitsBidsCount,
+    adUnitsNoBidsCount,
+    adUnitsBidders,
+    getPrebidData,
+    getPAData,
+  };
+};
+
+export default useDataProcessing;
---
+++ packages/extension/src/view/devtools/pages/privateAdvertising/protectedAudience/auctions/v2/hooks/useSidebarProcesing.tsx
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies.
+ */
+import type { SidebarItems } from '@google-psat/design-system';
+import AdUnits from '../../../adUnits';
+import { useEffect, useState } from 'react';
+import useDataProcessing from './useDataProcessing';
+import AdunitPanel from '../../components/adunitPanel';
+
+const useSidebarProcessing = () => {
+  const [sidebarData, setSidebarData] = useState<SidebarItems>({
+    adunits: {
+      title: 'Ad Units',
+      panel: {
+        Element: AdUnits,
+        props: {},
+      },
+      children: {},
+      dropdownOpen: true,
+    },
+  });
+
+  const {
+    adUnits,
+    adUnitsAuctionId,
+    adUnitsMediaContainerSize,
+    adUnitsBidsCount,
+    adUnitsNoBidsCount,
+    adUnitsBidders,
+    adUnitsTimestamp,
+    getPAData,
+    getPrebidData,
+  } = useDataProcessing();
+
+  useEffect(() => {
+    setSidebarData((prevSidebarData) => {
+      const newSidebarData = { ...prevSidebarData };
+
+      const adUnitContainerChildren = { ...newSidebarData.adunits.children };
+
+      adUnits.forEach((adUnit) => {
+        const mediaContainerSize = adUnitsMediaContainerSize[adUnit];
+        const bidders = adUnitsBidders[adUnit];
+        const biddersCount = bidders.length;
+        const bidsCount = adUnitsBidsCount[adUnit];
+        const noBidsCount = adUnitsNoBidsCount[adUnit];
+
+        adUnitContainerChildren[adUnit] = {
+          title: adUnit,
+          panel: {
+            Element: AdunitPanel,
+            props: {
+              adunit: adUnit,
+              mediaContainerSize,
+              bidders,
+              biddersCount,
+              bidsCount,
+              noBidsCount,
+            },
+          },
+          children: {},
+          dropdownOpen: true,
+        };
+
+        const adUnitChildren = { ...adUnitContainerChildren[adUnit].children };
+
+        adUnitsTimestamp[adUnit]?.forEach((time) => {
+          adUnitChildren[`${time}||${adUnit}`] = {
+            title: time,
+            children: {},
+          };
+
+          const timeChildren = {
+            ...adUnitChildren[`${time}||${adUnit}`].children,
+          };
+          timeChildren['Prebid'] = {
+            title: 'Prebid',
+            panel: {
+              props: {
+                auctionEvents: getPrebidData(adUnit, time),
+              },
+            },
+            children: {},
+          };
+
+          timeChildren['Adserver'] = {
+            title: 'Adserver',
+            children: {},
+          };
+
+          const PAData = getPAData(
+            adUnit,
+            time,
+            adUnitsAuctionId[adUnit][time]
+          );
+
+          const key = `${time}||${adUnitsAuctionId[adUnit][time]}||${adUnit}`;
+
+          timeChildren[key] = {
+            title: 'PAAPI',
+            panel: {
+              props: {
+                auctionEvents: PAData[key].auctionEvents,
+                parentOrigin: PAData[key].parentOrigin,
+                startDate: PAData[key].startDate,
+                isBlurred: PAData[key].isBlurred,
+              },
+            },
+            children: {
+              ...Object.entries(PAData[key].children).reduce(
+                (acc, [childKey, childValue]: [string, any]) => {
+                  acc[childKey] = {
+                    title: childValue.title,
+                    panel: {
+                      props: {
+                        auctionEvents: childValue.auctionEvents,
+                        parentOrigin: childValue.parentOrigin,
+                        startDate: childValue.startDate,
+                        isBlurred: childValue.isBlurred,
+                      },
+                    },
+                    children: {},
+                  };
+                  return acc;
+                },
+                {} as SidebarItems
+              ),
+            },
+          };
+
+          adUnitChildren[`${time}||${adUnit}`].children = {
+            ...timeChildren,
+          };
+        });
+
+        adUnitContainerChildren[adUnit].children = {
+          ...adUnitChildren,
+        };
+      });
+
+      newSidebarData.adunits.children = {
+        ...adUnitContainerChildren,
+      };
+
+      return newSidebarData;
+    });
+  }, [
+    adUnits,
+    adUnitsAuctionId,
+    adUnitsBidders,
+    adUnitsBidsCount,
+    adUnitsMediaContainerSize,
+    adUnitsNoBidsCount,
+    adUnitsTimestamp,
+    getPAData,
+    getPrebidData,
+  ]);
+
+  return {
+    sidebarData,
+  };
+};
+
+export default useSidebarProcessing;
