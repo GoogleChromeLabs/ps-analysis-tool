eccb41d2e5ab3951dc3f90fef8d071a4f007082d
Mayank Rana <mayankranax1@gmail.com>
2025-05-22 16:44:28 +0530

    Make adunit panel dumb and move to components folder

---
+++ packages/extension/src/view/devtools/pages/privateAdvertising/protectedAudience/auctions/components/adunitPanel/index.tsx
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { useTabs } from '@google-psat/design-system';
+
+/**
+ * Internal dependencies
+ */
+import {
+  useCookie,
+  useProtectedAudience,
+} from '../../../../../../stateProviders';
+import Panel from './panel';
+
+interface AdunitPanelProps {
+  adunit: string;
+  mediaContainerSize: number[][];
+  bidders: string[];
+  biddersCount: number;
+  bidsCount: number;
+  noBidsCount: number;
+}
+
+const AdunitPanel = ({
+  adunit,
+  mediaContainerSize,
+  bidders,
+  biddersCount,
+  bidsCount,
+  noBidsCount,
+}: AdunitPanelProps) => {
+  const { isInspecting, setIsInspecting } = useCookie(({ state, actions }) => ({
+    isInspecting: state.isInspecting,
+    setIsInspecting: actions.setIsInspecting,
+  }));
+
+  const { setSelectedAdUnit } = useProtectedAudience(({ actions }) => ({
+    setSelectedAdUnit: actions.setSelectedAdUnit,
+  }));
+
+  const { setStorage, setActiveTab } = useTabs(({ actions }) => ({
+    setStorage: actions.setStorage,
+    setActiveTab: actions.setActiveTab,
+  }));
+
+  return (
+    <Panel
+      adunit={adunit}
+      mediaContainerSize={mediaContainerSize}
+      bidders={bidders}
+      biddersCount={biddersCount}
+      bidsCount={bidsCount}
+      noBidsCount={noBidsCount}
+      isInspecting={isInspecting}
+      setIsInspecting={setIsInspecting}
+      setSelectedAdUnit={setSelectedAdUnit}
+      setStorage={setStorage}
+      setActiveTab={setActiveTab}
+    />
+  );
+};
+
+export default AdunitPanel;
---
+++ packages/extension/src/view/devtools/pages/privateAdvertising/protectedAudience/auctions/components/adunitPanel/panel.tsx
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import React, { useEffect, useMemo } from 'react';
+import { FrameIcon, MoneyIcon, ScreenIcon } from '@google-psat/design-system';
+
+/**
+ * Internal dependencies
+ */
+import Tile from './tile';
+import MatrixHOC from './matrixContainer';
+
+interface PanelProps {
+  adunit: string;
+  mediaContainerSize?: number[][];
+  bidders?: string[];
+  biddersCount: number;
+  bidsCount: number;
+  noBidsCount: number;
+  isInspecting?: boolean;
+  setIsInspecting?: React.Dispatch<React.SetStateAction<boolean>>;
+  setSelectedAdUnit?: React.Dispatch<React.SetStateAction<string | null>>;
+  setStorage?: (data: string, index?: number) => void;
+  setActiveTab?: (tab: number) => void;
+}
+
+const Panel = ({
+  adunit,
+  mediaContainerSize,
+  bidders,
+  biddersCount,
+  bidsCount,
+  noBidsCount,
+  isInspecting,
+  setIsInspecting,
+  setSelectedAdUnit,
+  setStorage,
+  setActiveTab,
+}: PanelProps) => {
+  const items = useMemo(
+    () => [
+      {
+        name: 'Ad Unit Code',
+        Icon: FrameIcon,
+        buttons: [
+          {
+            name: adunit,
+            onClick: () => {
+              if (isInspecting) {
+                setIsInspecting?.(false);
+                setSelectedAdUnit?.(null);
+              } else {
+                setIsInspecting?.(true);
+                setSelectedAdUnit?.(adunit);
+              }
+            },
+          },
+        ],
+      },
+      {
+        name: 'Ad Container Sizes',
+        Icon: ScreenIcon,
+        buttons: [
+          ...(mediaContainerSize || []).map((size) => ({
+            name: `${size?.[0]}x${size?.[1]}`,
+          })),
+        ],
+      },
+      {
+        name: 'Bidders',
+        Icon: MoneyIcon,
+        buttons: [
+          ...(bidders || []).map((bidder) => ({
+            name: bidder,
+            onClick: () => {
+              setStorage?.(
+                JSON.stringify({
+                  bidder,
+                  adUnitCode: adunit,
+                })
+              );
+              setActiveTab?.(5);
+            },
+          })),
+        ],
+      },
+    ],
+    [
+      adunit,
+      bidders,
+      mediaContainerSize,
+      isInspecting,
+      setActiveTab,
+      setIsInspecting,
+      setSelectedAdUnit,
+      setStorage,
+    ]
+  );
+
+  useEffect(() => {
+    return () => {
+      setIsInspecting?.(false);
+      setSelectedAdUnit?.(null);
+    };
+  }, [setIsInspecting, setSelectedAdUnit, adunit]);
+
+  return (
+    <>
+      <MatrixHOC
+        biddersCount={biddersCount}
+        bidsCount={bidsCount}
+        noBidsCount={noBidsCount}
+      />
+      <div className="p-4 flex gap-4 flex-wrap">
+        {items.map((item) => (
+          <Tile key={item.name} item={item} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Panel;
