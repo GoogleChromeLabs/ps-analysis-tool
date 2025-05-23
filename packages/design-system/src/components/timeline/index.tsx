b8ac701cf29fe795ceac622255e870ff15e1c459
sayedtaqui <sayedwp@gmail.com>
2025-05-23 18:48:21 +0530

    Add bars

--- packages/design-system/src/components/timeline/index.tsx
+++ packages/design-system/src/components/timeline/index.tsx
@@ -22,6 +22,46 @@ const LINE_COUNT = 10;
 const INITIAL_TIME = 50;
 const TIME_DURATION = 50;
 
+const BAR_COLORS = {
+  bid: '#7CACF8',
+  'no-bid': '#EC7159',
+  won: '#5CC971',
+  'timed-out': '#FC2D04',
+};
+
+const bars = [
+  {
+    name: 'Pubmattic',
+    duration: '270.1',
+    type: 'bid',
+  },
+  {
+    name: 'Sharethrough',
+    duration: '210.4',
+    type: 'no-bid',
+  },
+  {
+    name: 'appnexus',
+    duration: '240.0',
+    type: 'no-bid',
+  },
+  {
+    name: 'ix',
+    duration: '380.1',
+    type: 'no-bid',
+  },
+  {
+    name: 'Rubicon',
+    duration: '125.51',
+    type: 'won',
+  },
+  {
+    name: 'Criteo',
+    duration: '470.05',
+    type: 'timed-out',
+  },
+];
+
 const Timeline = () => {
   const lines = Array.from({ length: LINE_COUNT });
 
@@ -31,19 +71,44 @@ const Timeline = () => {
         <p>Auction Start: 12:18:27</p>
         <p>Auction Time: 380ms</p>
       </header>
-      <div className="h-[300px] flex justify-evenly border-pale-cornflower-blue border-1 mt-2">
-        {lines.map((_, index) => {
-          return (
-            <div
-              className="border-pale-cornflower-blue border-1 h-full relative"
-              key={index}
-            >
-              <span className="absolute right-0 pr-2 block text-xs">
-                {INITIAL_TIME + index * TIME_DURATION}ms
-              </span>
-            </div>
-          );
-        })}
+      <div className="h-[300px] border-pale-cornflower-blue border-1 mt-2 relative">
+        <div className="flex h-full">
+          {lines.map((_, index) => {
+            return (
+              <div
+                className="border-pale-cornflower-blue border-r-1 h-full w-[100px] relative"
+                key={index}
+              >
+                <span className="absolute right-0 pr-2 block text-xs">
+                  {INITIAL_TIME + index * TIME_DURATION}ms
+                </span>
+              </div>
+            );
+          })}
+        </div>
+        <div className="absolute top-0 left-0 w-full h-full">
+          <div className="relative">
+            {bars.map((bar, index) => {
+              const barWidth = parseFloat(bar.duration) * 2;
+              return (
+                <div
+                  key={index}
+                  className="absolute left-0 h-[10px] "
+                  style={{
+                    width: `${barWidth}px`,
+                    backgroundColor: BAR_COLORS[bar.type],
+                    top: `${(index + 1) * 40}px`,
+                  }}
+                >
+                  <div className="absolute left-0 bottom-[-20px] w-full flex justify-between px-1">
+                    <span className="pr-2 block text-xs">{bar.name}</span>
+                    <span className="text-xs">{bar.duration}ms</span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
