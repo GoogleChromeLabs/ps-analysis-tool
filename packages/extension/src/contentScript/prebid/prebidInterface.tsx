a29469298ee0153d4589253305617039fdd86294
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2025-06-23 12:59:57 +0530

    Add media type to BidAvailable event data in PrebidInterface

--- packages/extension/src/contentScript/prebid/prebidInterface.tsx
+++ packages/extension/src/contentScript/prebid/prebidInterface.tsx
@@ -278,6 +278,7 @@ class PrebidInterface {
               ).toISOString(),
               adUnitCode: args.adUnitCode,
               type: '',
+              adType: args.mediaType ?? '',
               eventType: 'BidAvailable',
             });
 
